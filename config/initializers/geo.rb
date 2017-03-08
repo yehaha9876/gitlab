4d4dfbbaf8e7d3bcd70549c4a26d528ac24b335a
Stan Hu <stanhu@gmail.com>
2017-03-08 01:43:24 -0800

    Comment out Geo init for testing

--- config/initializers/geo.rb
+++ config/initializers/geo.rb
@@ -1,5 +1,5 @@
 if File.exist?(Rails.root.join('config/database_geo.yml'))
   Rails.application.configure do
-    config.geo_database = config_for(:database_geo)
+#    config.geo_database = config_for(:database_geo)
   end
 end
