43e7d06d3134cd72bf226e6f41a79f68c3639c99
Bryce Johnson <bryce@gitlab.com>
2017-04-05 17:16:47 -0400

    Return import order to match CE.

--- app/assets/javascripts/vue_merge_request_widget/dependencies.js
+++ app/assets/javascripts/vue_merge_request_widget/dependencies.js
@@ -26,22 +26,22 @@ export { default as ConflictsState } from './components/states/mr_widget_conflic
 export { default as NothingToMergeState } from './components/states/mr_widget_nothing_to_merge';
 export { default as MissingBranchState } from './components/states/mr_widget_missing_branch';
 export { default as NotAllowedState } from './components/states/mr_widget_not_allowed';
+export { default as ReadyToMergeState } from './ee/components/states/mr_widget_ready_to_merge';
 export { default as UnresolvedDiscussionsState } from './components/states/mr_widget_unresolved_discussions';
 export { default as PipelineBlockedState } from './components/states/mr_widget_pipeline_blocked';
 export { default as PipelineFailedState } from './components/states/mr_widget_pipeline_failed';
 export { default as MergeWhenPipelineSucceedsState } from './components/states/mr_widget_merge_when_pipeline_succeeds';
 export { default as CheckingState } from './components/states/mr_widget_checking';
+export { default as MRWidgetStore } from './ee/stores/mr_widget_store';
+export { default as MRWidgetService } from './ee/services/mr_widget_service';
 export { default as eventHub } from './event_hub';
+export { default as deviseState } from './ee/stores/devise_state';
+export { default as mrWidgetOptions } from './ee/mr_widget_options';
+export { default as StateMaps } from './ee/stores/state_maps'; // TODO: Backport to CE
+export { default as SquashBeforeMerge } from './ee/components/states/mr_widget_squash_before_merge'; // TODO: Backport to CE
 
 /* EE Only */
 
-export { default as StateMaps } from './ee/stores/state_maps';
-export { default as ReadyToMergeState } from './ee/components/states/mr_widget_ready_to_merge';
-export { default as SquashBeforeMerge } from './ee/components/states/mr_widget_squash_before_merge';
-export { default as deviseState } from './ee/stores/devise_state';
 export { default as SecondaryGeoNode } from './ee/components/states/mr_widget_secondary_geo_node';
 export { default as WidgetApprovals } from './ee/components/approvals/mr_widget_approvals';
-export { default as mrWidgetOptions } from './ee/mr_widget_options';
-export { default as MRWidgetStore } from './ee/stores/mr_widget_store';
-export { default as MRWidgetService } from './ee/services/mr_widget_service';
 
