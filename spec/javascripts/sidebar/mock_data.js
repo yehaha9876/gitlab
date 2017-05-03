9700ce4ff11cff4a084af9dc12c20a091e436358
Jose Ivan Vargas <jvargas@gitlab.com>
2017-05-03 16:17:38 -0500

    [skip ci] Added sidebar store spec

--- spec/javascripts/sidebar/mock_data.js
+++ spec/javascripts/sidebar/mock_data.js
@@ -1,4 +1,10 @@
 export default {
+  time: {
+    time_estimate: 3600,
+    total_time_spent: 0,
+    human_time_estimate: '1h',
+    human_total_time_spent: null,
+  },
   user: {
     avatarUrl: 'http://gravatar.com/avatar/e64c7d89f26bd1972efa854d13d7dd61?s=80&d=identicon',
     id: 1,
