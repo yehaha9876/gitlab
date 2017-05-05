a3432a53021a929bdf10b1996c3d9eea2115664e
Clement Ho <ClemMakesApps@gmail.com>
2017-05-05 13:12:47 -0500

    Add specs for collapsed assignee

--- spec/javascripts/sidebar/assignees_spec.js
+++ spec/javascripts/sidebar/assignees_spec.js
@@ -12,6 +12,22 @@ describe('Assignee component', () => {
   });
 
   describe('No assignees/users', () => {
+    it('displays no assignee icon when collapsed', () => {
+      component = new AssigneeComponent({
+        propsData: {
+          rootPath: 'http://localhost:3000',
+          users: [],
+          editable: false,
+        },
+      }).$mount();
+
+      const collapsed = component.$el.querySelector('.sidebar-collapsed-icon');
+      expect(collapsed.childElementCount).toEqual(1);
+      expect(collapsed.children[0].getAttribute('aria-label')).toEqual('No Assignee');
+      expect(collapsed.children[0].classList.contains('fa')).toEqual(true);
+      expect(collapsed.children[0].classList.contains('fa-user')).toEqual(true);
+    });
+
     it('displays only "No assignee" when no users are assigned and the issue is read-only', () => {
       component = new AssigneeComponent({
         propsData: {
@@ -56,6 +72,25 @@ describe('Assignee component', () => {
   });
 
   describe('One assignee/user', () => {
+    it('displays one assignee icon when collapsed', () => {
+      component = new AssigneeComponent({
+        propsData: {
+          rootPath: 'http://localhost:3000',
+          users: [
+            UsersMock.user,
+          ],
+          editable: false,
+        },
+      }).$mount();
+
+      const collapsed = component.$el.querySelector('.sidebar-collapsed-icon');
+      const assignee = collapsed.children[0];
+      expect(collapsed.childElementCount).toEqual(1);
+      expect(assignee.querySelector('.avatar').getAttribute('src')).toEqual(UsersMock.user.avatarUrl);
+      expect(assignee.querySelector('.avatar').getAttribute('alt')).toEqual(`${UsersMock.user.name}'s avatar`);
+      expect(assignee.querySelector('.author').innerText.trim()).toEqual(UsersMock.user.name);
+    });
+
     it('Shows one user with avatar, username and author name', () => {
       component = new AssigneeComponent({
         propsData: {
@@ -92,6 +127,52 @@ describe('Assignee component', () => {
   });
 
   describe('Two or more assignees/users', () => {
+    it('displays two assignee icons when collapsed', () => {
+      const users = UsersMockHelper.createNumberRandomUsers(2);
+      component = new AssigneeComponent({
+        propsData: {
+          rootPath: 'http://localhost:3000',
+          users,
+          editable: false,
+        },
+      }).$mount();
+
+      const collapsed = component.$el.querySelector('.sidebar-collapsed-icon');
+      expect(collapsed.childElementCount).toEqual(2);
+
+      const first = collapsed.children[0];
+      expect(first.querySelector('.avatar').getAttribute('src')).toEqual(users[0].avatarUrl);
+      expect(first.querySelector('.avatar').getAttribute('alt')).toEqual(`${users[0].name}'s avatar`);
+      expect(first.querySelector('.author').innerText.trim()).toEqual(users[0].name);
+
+      const second = collapsed.children[1];
+      expect(second.querySelector('.avatar').getAttribute('src')).toEqual(users[1].avatarUrl);
+      expect(second.querySelector('.avatar').getAttribute('alt')).toEqual(`${users[1].name}'s avatar`);
+      expect(second.querySelector('.author').innerText.trim()).toEqual(users[1].name);
+    });
+
+    it('displays one assignee icon and counter when collapsed', () => {
+      const users = UsersMockHelper.createNumberRandomUsers(3);
+      component = new AssigneeComponent({
+        propsData: {
+          rootPath: 'http://localhost:3000',
+          users,
+          editable: false,
+        },
+      }).$mount();
+
+      const collapsed = component.$el.querySelector('.sidebar-collapsed-icon');
+      expect(collapsed.childElementCount).toEqual(2);
+
+      const first = collapsed.children[0];
+      expect(first.querySelector('.avatar').getAttribute('src')).toEqual(users[0].avatarUrl);
+      expect(first.querySelector('.avatar').getAttribute('alt')).toEqual(`${users[0].name}'s avatar`);
+      expect(first.querySelector('.author').innerText.trim()).toEqual(users[0].name);
+
+      const second = collapsed.children[1];
+      expect(second.querySelector('.avatar-counter').innerText.trim()).toEqual('+2');
+    });
+
     it('Shows two assignees', () => {
       const users = UsersMockHelper.createNumberRandomUsers(2);
       component = new AssigneeComponent({
