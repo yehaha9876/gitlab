fe30ea21f071916c3709c744947303628ded6d9a
Kamil Trzciński <ayufan@ayufan.eu>
2017-05-10 21:59:49 +0000

    Merge branch 'enforce-current-user-usage-on-entities-ee' into 'master'
    Make all Serializers receive `current_user` instead `user` [EE]
    
    See merge request !1869

--- app/serializers/build_entity.rb
+++ app/serializers/build_entity.rb
@@ -30,7 +30,7 @@ class BuildEntity < Grape::Entity
   end
 
   def detailed_status
-    build.detailed_status(request.user)
+    build.detailed_status(request.current_user)
   end
 
   def path_to(route, build)
