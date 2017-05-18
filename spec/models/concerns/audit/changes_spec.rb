6734844e97d0fc2b599481e3ef69e68042d6cc78
James Lopez <james@jameslopez.es>
2017-05-18 10:54:58 +0200

    add audit changes spec

---
+++ spec/models/concerns/audit/changes_spec.rb
@@ -0,0 +1,25 @@
+require 'spec_helper'
+
+describe Audit::Changes do
+  before do
+    stub_const 'FooUser', create(:user)
+    FooUser.class_eval{ include described_class }
+    FooUser.class_eval{ audit_changes :email, as: 'email_address' }
+  end
+
+  describe "non audit changes" do
+    it 'does not call the audit event service' do
+      expect(AuditEventService).not_to receive(:new)
+
+      FooUser.update(name: 'new name')
+    end
+  end
+
+  describe "audit changes" do
+    it 'calls the audit event service' do
+      expect(AuditEventService).to receive(:new)
+
+      FooUser.update(email: 'new@email.com')
+    end
+  end
+end
