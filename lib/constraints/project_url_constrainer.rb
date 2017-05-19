7b870fa7cb7c2a3629b7fe85efb35294f6b5daf5
Douwe Maan <douwe@selenight.nl>
2017-05-19 10:44:48 -0500

    Remove debug statement

--- lib/constraints/project_url_constrainer.rb
+++ lib/constraints/project_url_constrainer.rb
@@ -3,7 +3,6 @@ class ProjectUrlConstrainer
     namespace_path = request.params[:namespace_id]
     project_path = request.params[:project_id] || request.params[:id]
     full_path = namespace_path + '/' + project_path
-    Rails.logger.info "Testing params: #{request.params}"
 
     return false unless DynamicPathValidator.valid?(full_path)
     Project.find_by_full_path(full_path, follow_redirects: request.get?).present?
