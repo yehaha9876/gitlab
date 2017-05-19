1ae322f1485889422e9a4978d712d8ab2c26c4f5
Douwe Maan <douwe@selenight.nl>
2017-05-19 09:13:55 -0500

    Fix conflicts in spec/controllers/groups_controller_spec.rb and spec/workers/project_cache_worker_spec.rb

--- spec/workers/project_cache_worker_spec.rb
+++ spec/workers/project_cache_worker_spec.rb
@@ -46,7 +46,18 @@ describe ProjectCacheWorker do
         worker.perform(project.id, %w(readme))
       end
 
-<<<<<<< HEAD
+      context 'with plain readme' do
+        it 'refreshes the method caches' do
+          allow(MarkupHelper).to receive(:gitlab_markdown?).and_return(false)
+          allow(MarkupHelper).to receive(:plain?).and_return(true)
+
+          expect_any_instance_of(Repository).to receive(:refresh_method_caches).
+                                                  with(%i(readme)).
+                                                  and_call_original
+          worker.perform(project.id, %w(readme))
+        end
+      end
+
       context 'when in Geo secondary node' do
         before do
           allow(Gitlab::Geo).to receive(:secondary?).and_return(true)
@@ -60,17 +71,6 @@ describe ProjectCacheWorker do
           expect_any_instance_of(Project).not_to receive(:update_commit_count)
 
           subject.perform(project.id)
-=======
-      context 'with plain readme' do
-        it 'refreshes the method caches' do
-          allow(MarkupHelper).to receive(:gitlab_markdown?).and_return(false)
-          allow(MarkupHelper).to receive(:plain?).and_return(true)
-
-          expect_any_instance_of(Repository).to receive(:refresh_method_caches).
-                                                  with(%i(readme)).
-                                                  and_call_original
-          worker.perform(project.id, %w(readme))
->>>>>>> ce/9-2-stable
         end
       end
     end
