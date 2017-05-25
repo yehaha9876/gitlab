91264b83b9a2b2f485310e2ec5e231cb6e802e9a
James Lopez <james@jameslopez.es>
2017-05-25 16:08:08 +0200

    audit email changes

--- app/models/email.rb
+++ app/models/email.rb
@@ -1,11 +1,14 @@
 class Email < ActiveRecord::Base
   include Sortable
+  include Audit::Changes
 
   belongs_to :user
 
   validates :user_id, presence: true
   validates :email, presence: true, uniqueness: true, email: true
   validate :unique_email, if: ->(email) { email.email_changed? }
+
+  audit_presence :email, as: 'email address'
 
   def email=(value)
     write_attribute(:email, value.downcase.strip)
