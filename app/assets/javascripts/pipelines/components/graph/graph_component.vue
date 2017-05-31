d430f63a8f53f4581672769ca922937572a0fc6e
Bryce Johnson <bryce@gitlab.com>
2017-05-31 15:47:48 -0400

    Move polling and visibility management out of component.

--- app/assets/javascripts/pipelines/components/graph/graph_component.vue
+++ app/assets/javascripts/pipelines/components/graph/graph_component.vue
@@ -1,9 +1,5 @@
 <script>
   /* global Flash */
-  import Visibility from 'visibilityjs';
-  import Poll from '../../../lib/utils/poll';
-  import PipelineService from '../../services/pipeline_service';
-  import PipelineStore from '../../stores/pipeline_store';
   import linkedPipelinesColumn from './linked_pipelines_column.vue';
   import stageColumnComponent from './stage_column_component.vue';
   import loadingIcon from '../../../vue_shared/components/loading_icon.vue';
@@ -15,43 +11,16 @@
       stageColumnComponent,
       loadingIcon,
     },
-
-    data() {
-      const DOMdata = document.getElementById('js-pipeline-graph-vue').dataset;
-      const store = new PipelineStore();
-
-      return {
-        isLoading: false,
-        endpoint: DOMdata.endpoint,
-        store,
-        state: store.state,
-      };
-    },
-
-    created() {
-      this.service = new PipelineService(this.endpoint);
-
-      const poll = new Poll({
-        resource: this.service,
-        method: 'getPipeline',
-        successCallback: this.successCallback,
-        errorCallback: this.errorCallback,
-      });
-
-      if (!Visibility.hidden()) {
-        this.isLoading = true;
-        poll.makeRequest();
-      }
-
-      Visibility.change(() => {
-        if (!Visibility.hidden()) {
-          poll.restart();
-        } else {
-          poll.stop();
-        }
-      });
+    props: {
+      state: {
+        type: Object,
+        required: true,
+      },
     },
     computed: {
+      isLoading() {
+        return !this.state.graph.length;
+      },
       hasTriggered() {
         return !!this.state.triggered.length;
       },
@@ -63,18 +32,6 @@
       },
     },
     methods: {
-      successCallback(response) {
-        const data = response.json();
-
-        this.isLoading = false;
-        this.store.storeGraph(data.details.stages);
-      },
-
-      errorCallback() {
-        this.isLoading = false;
-        return new Flash('An error occurred while fetching the pipeline.');
-      },
-
       capitalizeStageName(name) {
         return name.charAt(0).toUpperCase() + name.slice(1);
       },
--- app/assets/javascripts/pipelines/graph_bundle.js
+++ app/assets/javascripts/pipelines/graph_bundle.js
@@ -1,10 +1,50 @@
 import Vue from 'vue';
+import Visibility from 'visibilityjs';
+import Poll from '../lib/utils/poll';
 import pipelineGraph from './components/graph/graph_component.vue';
+import PipelineService from './services/pipeline_service';
+import PipelineStore from './stores/pipeline_store';
 
-document.addEventListener('DOMContentLoaded', () => new Vue({
-  el: '#js-pipeline-graph-vue',
-  components: {
-    pipelineGraph,
-  },
-  render: createElement => createElement('pipeline-graph'),
-}));
+document.addEventListener('DOMContentLoaded', () => {
+  const DOMdata = document.getElementById('js-pipeline-graph-vue').dataset;
+  const store = new PipelineStore();
+  const service = new PipelineService(DOMdata.endpoint);
+
+  new Vue({
+    el: '#js-pipeline-graph-vue',
+    components: {
+      pipelineGraph,
+    },
+    data() {
+      return {
+        state: store.state,
+      };
+    },
+    created() {
+      const poll = new Poll({
+        resource: service,
+        method: 'getPipeline',
+        successCallback: (response) => {
+          store.storeGraph(response.json().details.stages)
+        },
+        errorCallback: () => new Flash('An error occurred while fetching the pipeline.'),
+      });
+
+      if (!Visibility.hidden()) {
+        poll.makeRequest();
+      }
+
+      Visibility.change(() => {
+        if (!Visibility.hidden()) {
+          poll.restart();
+        } else {
+          poll.stop();
+        }
+      });
+    },
+    template: `
+      <pipeline-graph :state="state"></pipeline-graph>
+    `,
+  });
+});
+
