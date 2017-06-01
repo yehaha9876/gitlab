1267f0f9772095d2dcef9463d47a8e0159e2d57f
Bryce Johnson <bryce@gitlab.com>
2017-06-01 16:01:09 -0400

    Remove connector before, when only one job.

--- app/assets/javascripts/pipelines/components/graph/graph_component.vue
+++ app/assets/javascripts/pipelines/components/graph/graph_component.vue
@@ -45,16 +45,19 @@
 
         // If it's the first stage column and only has one job
         if (index === 0 && stage.groups.length === 1) {
+          // and no upstream pipelines
           if (!this.hasTriggerer) {
             className = 'no-margin';
           } else {
             className = 'left-margin';
           }
+        // if there is only one job in the stage
+        } else if (stage.groups.length === 1) {
+          className = '';
         } else if (index > 0) {
           // If it is not the first column
           className = 'left-margin';
         }
-
         return className;
       },
       linkedPipelineClass(index) {
