f38090d3ef0ee5a86d2809b1dd30832d62a684b5
Douwe Maan <douwe@gitlab.com>
2017-06-07 21:41:56 +0000

    Merge branch 'da-fix-issue-2603' into 'master'
    Check if database_geo.yml exist before setting the tracking DB config
    
    Closes #2603
    
    See merge request !2071

--- config/initializers/geo.rb
+++ config/initializers/geo.rb
@@ -1,4 +1,5 @@
-if File.exist?(Rails.root.join('config/database_geo.yml'))
+if File.exist?(Rails.root.join('config/database_geo.yml')) &&
+    Gitlab::Geo.secondary_role_enabled?
   Rails.application.configure do
     config.geo_database = config_for(:database_geo)
   end
