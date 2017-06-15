e84edc1d3dda53a732a315d91d30a822bc338e3e
Lin Jen-Shin <godfat@godfat.org>
2017-06-15 17:18:48 +0800

    Resolve spec/serializers/pipeline_serializer_spec.rb

--- spec/serializers/pipeline_serializer_spec.rb
+++ spec/serializers/pipeline_serializer_spec.rb
@@ -113,11 +113,7 @@ describe PipelineSerializer do
 
       it "verifies number of queries" do
         recorded = ActiveRecord::QueryRecorder.new { subject }
-<<<<<<< HEAD
         expect(recorded.count).to be_within(1).of(64)
-=======
-        expect(recorded.count).to be_within(1).of(60)
->>>>>>> 0d9311624754fbc3e0b8f4a28be576e48783bf81
         expect(recorded.cached_count).to eq(0)
       end
 
