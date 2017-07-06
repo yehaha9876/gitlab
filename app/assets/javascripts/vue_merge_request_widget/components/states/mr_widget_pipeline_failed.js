352de5a13240dd96c450560d623bf2362aeeb752
Simon Knox <psimyn@gmail.com>
2017-07-06 10:30:56 +1000

    fix package version

--- app/assets/javascripts/vue_merge_request_widget/components/states/mr_widget_pipeline_failed.js
+++ app/assets/javascripts/vue_merge_request_widget/components/states/mr_widget_pipeline_failed.js
@@ -9,15 +9,16 @@ export default {
     <div class="mr-widget-body media">
       <ci-icon :status="{ group: 'failed', icon: 'icon_status_failed' }" />
       <div class="media-body">
-      <span class="bold">
-        The pipeline for this merge request failed. Please retry the job or push a new commit to fix the failure
-      </span>
-      <button
-        class="btn btn-success btn-xs"
-        disabled="true"
-        type="button">
-        Merge
-      </button>
+        <span class="bold">
+          The pipeline for this merge request failed. Please retry the job or push a new commit to fix the failure
+        </span>
+        <button
+          class="btn btn-success btn-xs"
+          disabled="true"
+          type="button">
+          Merge
+        </button>
+      </div>
     </div>
   `,
 };
