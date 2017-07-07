7dd079d13c9b273ee3de80fef58bcb80d57c1d0c
Simon Knox <psimyn@gmail.com>
2017-07-07 13:50:38 +1000

    remove storybook, moved it to mr-storybook branch

--- .storybook/config.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable import/no-extraneous-dependencies, import/no-unresolved, import/extensions */
-
-import { configure } from '@storybook/vue'
-
-function loadStories() {
-  require('../stories')
-}
-
-configure(loadStories, module)
