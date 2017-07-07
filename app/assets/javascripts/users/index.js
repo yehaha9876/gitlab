6fd9973f16f2e79de5b37cb3157816f84cf5c02e
Mike Greiling <mike@pixelcog.com>
2017-07-07 16:33:30 -0500

    refactor Calendar class to ES module syntax

--- app/assets/javascripts/users/index.js
+++ app/assets/javascripts/users/index.js
@@ -1,5 +1,7 @@
-import './calendar';
+import ActivityCalendar from './activity_calendar';
 import User from './user';
 
+// use legacy exports until embedded javascript is refactored
+window.Calendar = ActivityCalendar;
 window.gl = window.gl || {};
 window.gl.User = User;
