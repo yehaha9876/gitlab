2c575dcac4b759a67f3f93ce36fa7e889e12b504
Simon Knox <psimyn@gmail.com>
2017-07-13 20:11:03 +1000

    header, deployment stories

--- stories/index.js
+++ stories/index.js
@@ -10,7 +10,10 @@ import '../app/assets/javascripts/commons/bootstrap';
 import '../app/assets/javascripts/flash';
 
 import './mr_widget_story';
+import './mr_widget_header_story';
 import './mr_widget_states_story';
+import './mr_widget_pipeline_story';
+import './mr_widget_deployment_story';
 
 try {
   require('./ee');
