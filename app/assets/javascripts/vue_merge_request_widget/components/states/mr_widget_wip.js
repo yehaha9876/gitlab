820b66f8e226906662b6221bab26cf50fa772a1f
Jose Ivan Vargas <jvargas@gitlab.com>
2017-08-08 15:39:16 -0500

    Fix linters

--- app/assets/javascripts/vue_merge_request_widget/components/states/mr_widget_wip.js
+++ app/assets/javascripts/vue_merge_request_widget/components/states/mr_widget_wip.js
@@ -39,9 +39,6 @@ export default {
         });
     },
   },
-  components: {
-    statusIcon,
-  },
   template: `
     <div class="mr-widget-body media">
       <status-icon status="failed" :showDisabledButton="Boolean(mr.removeWIPPath)" />
