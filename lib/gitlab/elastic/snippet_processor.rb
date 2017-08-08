44a185a86a4c0065428f01cf229d4b5d71310a87
Valery Sizov <valery@gitlab.com>
2017-08-08 14:57:38 +0300

    Fix snippet clining[ci skip]

--- lib/gitlab/elastic/snippet_processor.rb
+++ lib/gitlab/elastic/snippet_processor.rb
@@ -1,14 +1,15 @@
 module Gitlab
   module Elastic
     class SnippetProcessor
-      def initialize(whole_document, snippet)
-        @whole_document = whole_document
+      def initialize(document, snippet)
+        @document = document
         @snippet = snippet
       end
 
       def startline
-        index = @whole_document.rindex(clean_snippet)
-        @whole_document[0, index].count("\n") + 1
+        index = @document.rindex(clean_snippet)
+        # byebug if index.nil?
+        @document[0, index].count("\n") + 1
       end
 
       # Returns the origin snippet without any highlight tags
@@ -24,10 +25,10 @@ module Gitlab
       private
 
       # Removes a first line if it does not have the highlight tags
-      # It's needed because ES can return not the full first code line which is
-      # unthinkable to keep as is.
+      # It's needed because ES can return non-complite code line which is
+      # unthinkable to keep as is. We should treat code lines as atomic entitties.
       def snippet_processed
-        @snippet_processed ||= @snippet.sub(/^((?!gitlabelasticsearch→).)*[\n\r]/, '')
+        @snippet_processed ||= @snippet.sub(/\A((?!gitlabelasticsearch→).)*[\n\r]/, '')
       end
     end
   end
