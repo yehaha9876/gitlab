b29fcb768ce9ee0c247df6456ad8decb1cca6fa2
Tim Zallmann <tzallmann@gitlab.com>
2017-08-14 10:32:00 +0000

    Merge branch 'user-edit-files-spec-fix' into 'master'
    Wait for requests to finish before running the ace JS
    
    Closes #36191
    
    See merge request !13405

--- spec/features/projects/user_edits_files_spec.rb
+++ spec/features/projects/user_edits_files_spec.rb
@@ -24,6 +24,9 @@ describe 'User edits files' do
     it 'inserts a content of a file', js: true do
       click_link('.gitignore')
       find('.js-edit-blob').click
+
+      wait_for_requests
+
       execute_script("ace.edit('editor').setValue('*.rbca')")
 
       expect(evaluate_script('ace.edit("editor").getValue()')).to eq('*.rbca')
@@ -39,7 +42,9 @@ describe 'User edits files' do
     it 'commits an edited file', js: true do
       click_link('.gitignore')
       find('.js-edit-blob').click
-      find('.file-editor', match: :first)
+
+      wait_for_requests
+
       execute_script("ace.edit('editor').setValue('*.rbca')")
       fill_in(:commit_message, with: 'New commit message', visible: true)
       click_button('Commit changes')
@@ -54,7 +59,9 @@ describe 'User edits files' do
     it 'commits an edited file to a new branch', js: true do
       click_link('.gitignore')
       find('.js-edit-blob').click
-      find('.file-editor', match: :first)
+
+      wait_for_requests
+
       execute_script("ace.edit('editor').setValue('*.rbca')")
       fill_in(:commit_message, with: 'New commit message', visible: true)
       fill_in(:branch_name, with: 'new_branch_name', visible: true)
@@ -71,7 +78,9 @@ describe 'User edits files' do
     it 'shows the diff of an edited file', js: true do
       click_link('.gitignore')
       find('.js-edit-blob').click
-      find('.file-editor', match: :first)
+
+      wait_for_requests
+
       execute_script("ace.edit('editor').setValue('*.rbca')")
       click_link('Preview changes')
 
@@ -96,7 +105,8 @@ describe 'User edits files' do
 
       expect(page).to have_content(fork_message)
 
-      find('.file-editor', match: :first)
+      wait_for_requests
+
       execute_script("ace.edit('editor').setValue('*.rbca')")
 
       expect(evaluate_script('ace.edit("editor").getValue()')).to eq('*.rbca')
@@ -110,7 +120,8 @@ describe 'User edits files' do
       expect(page).to have_button('Cancel')
 
       click_link('Fork')
-      find('.file-editor', match: :first)
+
+      wait_for_requests
 
       execute_script("ace.edit('editor').setValue('*.rbca')")
       fill_in(:commit_message, with: 'New commit message', visible: true)
