544c785b159f54c973dfa994c47628a3e427774e
Clement Ho <ClemMakesApps@gmail.com>
2017-08-18 17:45:37 -0500

    [skip ci] add feature highlight

---
+++ app/assets/javascripts/feature_highlight.js
@@ -0,0 +1,66 @@
+import Cookies from 'js-cookie';
+
+const highlightOrder = ['issue-list', 'issue-boards'];
+
+const mouseenter = function() {
+  const $featureHighlight = $(this);
+  $featureHighlight.popover('show');
+  $('.popover').on('mouseleave', () => $featureHighlight.popover('hide'));
+}
+
+const mouseleave = function() {
+  const $featureHighlight = $(this);
+  if (!$('.popover:hover').length) {
+    $featureHighlight.popover('hide');
+  }
+}
+
+const dismiss = function(cookieId) {
+  Cookies.set(`feature-highlighted-${cookieId}`, true);
+  this.popover('hide');
+  this.hide();
+}
+
+const setupDismissButton = function() {
+  const popoverId = this.getAttribute('aria-describedby');
+  const cookieId = this.dataset.highlight;
+  const $popover = $(this);
+
+  document.querySelector(`#${popoverId} .dismiss-feature-highlight`)
+    .addEventListener('click', dismiss.bind($popover, cookieId));
+}
+
+const initFeatureHighlight = (id) => {
+  const $selector = $(`.js-feature-highlight[data-highlight=${id}]`);
+  const $parent = $selector.parent();
+  const $popoverContent = $parent.siblings('.feature-highlight-popover-content');
+
+  // Setup popover
+  $selector.data('content', $popoverContent[0].outerHTML);
+  $selector.popover({ html: true });
+
+  $selector.on('mouseenter', mouseenter);
+  $selector.on('mouseleave', mouseleave);
+  $selector.on('inserted.bs.popover', setupDismissButton);
+
+  // Override css as we are now ready to explicitly show
+  // the feature highlight
+  $selector.show();
+}
+
+const featureHighlightManager = () => {
+  highlightOrder.some((id, index) => {
+    const element = document.querySelector(`.js-feature-highlight[data-highlight=${id}]`);
+    const previouslyDismissed = Cookies.get(`feature-highlighted-${id}`) === 'true';
+    if (element && !previouslyDismissed) {
+      initFeatureHighlight(id);
+      return true;
+    }
+
+    return false;
+  });
+};
+
+featureHighlightManager();
+
+export default featureHighlightManager;
