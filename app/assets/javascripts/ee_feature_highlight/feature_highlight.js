1e89d4d66bd24bceb6d8a486d128af4610d27a51
Clement Ho <ClemMakesApps@gmail.com>
2017-09-08 15:28:48 -0500

    Pick changes from CE MR 14165

---
+++ app/assets/javascripts/ee_feature_highlight/feature_highlight.js
@@ -0,0 +1,8 @@
+import { init } from '../feature_highlight/feature_highlight';
+
+const highlightOrder = ['service-desk', 'issue-boards'];
+
+export {
+  highlightOrder,
+  init,
+};
--- app/assets/javascripts/ee_feature_highlight/feature_highlight_options.js
+++
@@ -1,8 +0,0 @@
-import { initHighlightOrder } from '../feature_highlight/feature_highlight_options';
-
-const highlightOrder = ['service-desk', 'issue-boards'];
-
-export {
-  highlightOrder,
-  initHighlightOrder,
-};
