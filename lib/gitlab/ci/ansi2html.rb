3860203ab6dd114327168b41e8582bb26a2d9258
Alessio Caiazza <acaiazza@gitlab.com>
2017-10-06 16:14:54 +0200

    Port of 37970-ci-sections-tracking to EE

--- lib/gitlab/ci/ansi2html.rb
+++ lib/gitlab/ci/ansi2html.rb
@@ -155,7 +155,9 @@ module Gitlab
           stream.each_line do |line|
             s = StringScanner.new(line)
             until s.eos?
-              if s.scan(/\e([@-_])(.*?)([@-~])/)
+              if s.scan(Gitlab::Regex.build_trace_section_regex)
+                handle_section(s)
+              elsif s.scan(/\e([@-_])(.*?)([@-~])/)
                 handle_sequence(s)
               elsif s.scan(/\e(([@-_])(.*?)?)?$/)
                 break
