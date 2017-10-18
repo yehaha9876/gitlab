edf9e209adbe6b7a206c3f80b8d634a9a37f299e
Rémy Coutable <remy@rymai.me>
2017-10-18 09:54:03 +0200

    Move EE::OauthApplications to its correct place
    Signed-off-by: Rémy Coutable <remy@rymai.me>

---
+++ ee/app/controllers/concerns/ee/oauth_applications.rb
@@ -0,0 +1,12 @@
+module EE
+  module OauthApplications
+    def log_audit_event
+      ::AuditEventService.new(current_user,
+                              current_user,
+                              action: :custom,
+                              custom_message: 'OAuth access granted',
+                              ip_address: request.remote_ip)
+          .for_user(@application.name).security_event
+    end
+  end
+end
