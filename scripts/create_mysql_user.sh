58689ecae296bff945b54efa8534c68d95aae513
Stan Hu <stanhu@gmail.com>
2017-11-08 14:41:06 -0800

    Use IF NOT EXISTS with creating MySQL users
    Disable creation of user in gitlab_git_test

--- scripts/create_mysql_user.sh
+++ scripts/create_mysql_user.sh
@@ -2,7 +2,7 @@
 
 mysql --user=root --host=mysql <<EOF
 CREATE DATABASE IF NOT EXISTS gitlabhq_test;
-CREATE USER 'gitlab'@'%';
+CREATE USER IF NOT EXISTS 'gitlab'@'%';
 GRANT ALL PRIVILEGES ON gitlabhq_test.* TO 'gitlab'@'%';
 FLUSH PRIVILEGES;
 EOF
