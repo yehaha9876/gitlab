7e96c12a7565614d92a6b16d9cac6176ccf2b777
Francisco Lopez <fjlopez@gitlab.com>
2017-11-16 17:11:17 +0100

    Port from CE to EE

--- app/models/identity.rb
+++ app/models/identity.rb
@@ -4,13 +4,14 @@ class Identity < ActiveRecord::Base
   belongs_to :user
 
   validates :provider, presence: true
-  validates :extern_uid, allow_blank: true, uniqueness: { scope: :provider }
+  validates :extern_uid, allow_blank: true, uniqueness: { scope: :provider, case_sensitive: false }
   validates :user_id, uniqueness: { scope: :provider }
 
   scope :with_provider, ->(provider) { where(provider: provider) }
   scope :with_extern_uid, ->(provider, extern_uid) do
-    extern_uid = Gitlab::LDAP::Person.normalize_dn(extern_uid) if provider.starts_with?('ldap')
-    where(extern_uid: extern_uid, provider: provider)
+    extern_uid = Gitlab::LDAP::Person.normalize_dn(extern_uid) if provider.to_s.starts_with?('ldap')
+
+    iwhere(extern_uid: extern_uid.to_s).where(provider: provider)
   end
 
   def ldap?
