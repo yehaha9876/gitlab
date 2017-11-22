4624daaf6a3b31301b208d3dbf68f03e86257cd2
Tim Zallmann <tzallmann@gitlab.com>
2017-11-22 17:16:14 +0100

    Changed Sortable to yarn Library

--- app/assets/javascripts/boards/components/board.js
+++ app/assets/javascripts/boards/components/board.js
@@ -1,6 +1,6 @@
 /* eslint-disable comma-dangle, space-before-function-paren, one-var */
-import Sortable from 'vendor/Sortable';
 import Vue from 'vue';
+import Sortable from 'sortablejs';
 import AccessorUtilities from '../../lib/utils/accessor';
 import boardList from './board_list.vue';
 import boardBlankState from './board_blank_state';
