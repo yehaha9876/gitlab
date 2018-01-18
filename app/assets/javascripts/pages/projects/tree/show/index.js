21a836ad28d88f3c451feb87c2952bc138420bca
Jose Ivan Vargas <jvargas@gitlab.com>
2018-01-18 16:22:20 -0600

    Added pipeline status to the files view

--- app/assets/javascripts/pages/projects/tree/show/index.js
+++ app/assets/javascripts/pages/projects/tree/show/index.js
@@ -1,9 +1,11 @@
+import Vue from 'vue';
 import initPathLocks from 'ee/path_locks';
 import TreeView from '../../../../tree';
 import ShortcutsNavigation from '../../../../shortcuts_navigation';
 import BlobViewer from '../../../../blob/viewer';
 import NewCommitForm from '../../../../new_commit_form';
 import { ajaxGet } from '../../../../lib/utils/common_utils';
+import commitPipelineStatus from '../components/commit_pipeline_status_component.vue';
 
 export default () => {
   new ShortcutsNavigation(); // eslint-disable-line no-new
@@ -12,6 +14,22 @@ export default () => {
   new NewCommitForm($('.js-create-dir-form')); // eslint-disable-line no-new
   $('#tree-slider').waitForImages(() =>
     ajaxGet(document.querySelector('.js-tree-content').dataset.logsPath));
+
+  const commitPipelineStatusEl = document.getElementById('commit-pipeline-status');
+  // eslint-disable-next-line no-new
+  new Vue({
+    el: '#commit-pipeline-status',
+    components: {
+      commitPipelineStatus,
+    },
+    render(createElement) {
+      return createElement('commit-pipeline-status', {
+        props: {
+          endpoint: commitPipelineStatusEl.dataset.endpoint,
+        },
+      });
+    },
+  });
 
   if (document.querySelector('.js-tree-content').dataset.pathLocksAvailable === 'true') {
     initPathLocks(
