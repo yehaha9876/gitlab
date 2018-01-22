8410c9b92ae56e930b382d6db5d57affe9f19efb
Shinya Maeda <shinya@gitlab.com>
2018-01-22 19:36:11 +0900

    Migrate old traces to artifacts

---
+++ spec/migrations/migrate_legacy_traces_to_ci_job_artifacts_spec.rb
@@ -0,0 +1,9 @@
+require 'spec_helper'
+require Rails.root.join('db', 'post_migrate', '20180122140220_migrate_legacy_traces_to_ci_job_artifacts.rb')
+
+describe MigrateLegacyTracesToCiJobArtifacts, :migration do
+  
+  it 'test' do
+
+  end
+end
