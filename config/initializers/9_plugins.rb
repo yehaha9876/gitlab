a9c5adeec4f7400ac268a26491951c302803bf1a
Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>
2018-01-30 17:45:35 +0200

    Add validation for plugins on startup
    Signed-off-by: Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>

--- config/initializers/9_plugins.rb
+++ config/initializers/9_plugins.rb
@@ -1,7 +1,33 @@
+class PluginsSystem
+  attr_accessor :plugins
+
+  def initialize
+    files = Rails.root.join('plugins', '*_service.rb')
+
+    @plugins = Dir.glob(files).map do |file|
+      File.basename(file).sub('_service.rb', '')
+    end
+  end
+
+  def valid_plugins
+    plugins.select do |plugin|
+      klass = Object.const_get("#{plugin.camelize}_service".classify)
+      instance = klass.new
+
+      # Just give sample data to method and expect it to not crash.
+      begin
+        klass.new.execute(Gitlab::DataBuilder::Push::SAMPLE_DATA)
+      rescue => e
+        Rails.logger.warn("GitLab: #{plugin} - plugin has problems. #{e}")
+        false
+      else
+        Rails.logger.info "GitLab: #{plugin} - plugin is enabled"
+        true
+      end
+    end
+  end
+end
+
 # Load external plugins from /plugins directory
 # and set into PLUGINS variable
-files = Rails.root.join('plugins', '*_service.rb')
-
-PLUGINS = Dir.glob(files).map do |file|
-  File.basename(file).sub('_service.rb', '')
-end
+PLUGINS = PluginsSystem.new.valid_plugins
