42bd712f86ef8ee84a4c335eccd760349bba873f
Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>
2018-01-30 16:38:10 +0200

    Add generator for custom plugins
    Signed-off-by: Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>

---
+++ generator_templates/plugins/template.rb
@@ -0,0 +1,40 @@
+# Before using custom plugins please consider other options:
+# * Webhooks
+# * API
+# * Existing integrations
+#
+# Requirements
+# * File name must end with _service.rb. For example, jenkins_service.rb.
+# * All code should be inside class. No code should be executed on file load.
+# * Class name must be same as file name.
+#   If file name is jenkins_service.rb then class name must be JenkinsService.
+# * Plugin class must inherit from Service class
+#
+# Reccomendations
+# * Code should not depend on or use GitLab classes and other code.
+# * Look at https://gitlab.com/gitlab-org/gitlab-ce/tree/master/app/models/project_services for examples.
+# * Use this snippet https://gitlab.com/snippets/1688058 as working example for plugin.
+# * Consider contributing your plugin to GitLab source code so we can test it
+#   and make sure it will work in further version.
+#
+class $NAMEService < Service
+  def title
+    '$NAME'
+  end
+
+  def description
+    'Replace me with the descrition of the plugin'
+  end
+
+  def self.to_param
+    '$PARAM'
+  end
+
+  def fields
+    []
+  end
+
+  def execute(data)
+    # TODO: Implement me
+  end
+end
---
+++ lib/tasks/plugins.rake
@@ -0,0 +1,35 @@
+namespace :plugins do
+  desc 'Generate sceleton for new plugin'
+  task generate: :environment do
+    ARGV.each { |a| task a.to_sym do ; end }
+    name = ARGV[1]
+
+    unless name.present?
+      puts 'Error. You need to specify a name for the plugin'
+      exit 1
+    end
+
+    plugins_dir =
+    class_name = name.classify
+    param = name.parameterize
+
+    if Service.available_services_names.include?(param)
+      puts 'Integration with such name is already availble in GitLab.'
+      puts 'Check it out or choose a different name for your plugin.'
+      exit 1
+    end
+
+    file_path = Rails.root.join('plugins', param + '_service.rb')
+
+    template = File.read(Rails.root.join('generator_templates', 'plugins', 'template.rb'))
+    template.gsub!('$NAME', class_name)
+    template.gsub!('$PARAM', param)
+
+    if File.write(file_path, template)
+      puts "Done. Your plugin saved under #{file_path}."
+      puts 'Feel free to edit it.'
+    else
+      puts "Failed to save #{file_path}."
+    end
+  end
+end
