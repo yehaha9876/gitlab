d316296a83d28d984ed26a9ac1e50480f102d525
Joshua Lambert <joshua@gitlab.com>
2018-02-19 16:29:46 -0500

    Utilize env so it compiles

--- config/initializers/tracing.rb
+++ config/initializers/tracing.rb
@@ -3,10 +3,13 @@ require 'jaeger/client'
 require 'rack/tracer'
 require 'rails/tracer'
 
-OpenTracing.global_tracer = Jaeger::Client.build(host: 'jaeger-agent.default.svc.cluster.local', port: 6831, service_name: 'gitlab')
 
-Rails::Rack::Tracer.instrument
-ActiveRecord::Tracer.instrument(tracer: OpenTracing.global_tracer, 
-    active_span: -> { OpenTracing.global_tracer.active_span })
-ActiveSupport::Cache::Tracer.instrument(tracer: OpenTracing.global_tracer, 
-    active_span: -> { OpenTracing.global_tracer.active_span })
+if(ENV['jaeger_tracing'] == 'true') 
+    OpenTracing.global_tracer = Jaeger::Client.build(host: ENV['jaeger_host'], port: 6831, service_name: 'gitlab')
+
+    Rails::Rack::Tracer.instrument
+    ActiveRecord::Tracer.instrument(tracer: OpenTracing.global_tracer, 
+        active_span: -> { OpenTracing.global_tracer.active_span })
+    ActiveSupport::Cache::Tracer.instrument(tracer: OpenTracing.global_tracer, 
+        active_span: -> { OpenTracing.global_tracer.active_span })
+end
