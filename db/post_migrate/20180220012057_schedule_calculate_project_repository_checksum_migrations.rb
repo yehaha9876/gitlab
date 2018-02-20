aac3af948b55cdf9cee184a3ee72f7d0568b6557
Douglas Barbosa Alexandre <dbalexandre@gmail.com>
2018-02-20 01:26:35 -0300

    Add a post migrate to schedule the CalculateProjectRepositoryChecksum in batches

---
+++ db/post_migrate/20180220012057_schedule_calculate_project_repository_checksum_migrations.rb
@@ -0,0 +1,34 @@
+class ScheduleCalculateProjectRepositoryChecksumMigrations < ActiveRecord::Migration
+  include Gitlab::Database::MigrationHelpers
+
+  DOWNTIME = false
+  BATCH_SIZE = 1_000
+  MIGRATION = 'CalculateProjectRepositoryChecksum'.freeze
+  DELAY_INTERVAL = 10.seconds # 5.minutes
+
+  disable_ddl_transaction!
+
+  class ProjectState < ActiveRecord::Base
+    self.table_name = 'project_states'
+  end
+
+  class Project < ActiveRecord::Base
+    self.table_name = 'projects'
+
+    include ::EachBatch
+  end
+
+  def up
+    relation = Project.joins("LEFT OUTER JOIN project_states on projects.id = project_states.project_id")
+                      .merge(ProjectState.where(project_id: nil))
+
+    relation.each_batch(of: BATCH_SIZE, column: :last_activity_at) do |batch, index|
+      range = batch.pluck('MIN(projects.id)', 'MAX(projects.id)').first
+
+      BackgroundMigrationWorker.perform_in(index * DELAY_INTERVAL, MIGRATION, range)
+    end
+  end
+
+  def down
+  end
+end
