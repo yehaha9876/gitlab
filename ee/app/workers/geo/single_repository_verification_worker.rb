9c614c39ed97c33546a13d71bbf782197039a411
Douglas Barbosa Alexandre <dbalexandre@gmail.com>
2018-02-22 16:57:17 -0300

    Refactoring SingleRepositoryVerificationWorker

--- ee/app/workers/geo/single_repository_verification_worker.rb
+++ ee/app/workers/geo/single_repository_verification_worker.rb
@@ -2,55 +2,46 @@ module Geo
   class SingleRepositoryVerificationWorker
     include ApplicationWorker
     include GeoQueue
-    include Gitlab::Geo::LogHelpers
+    include ExclusiveLeaseGuard
+    include Gitlab::Geo::ProjectLogHelpers
 
     LEASE_TIMEOUT = 1.hour.to_i
+
+    attr_reader :project
 
     def perform(project_id)
       return unless Gitlab::Geo.primary?
 
-      project = Project.find_by(id: project_id)
+      @project = Project.find_by(id: project_id)
       return if project.nil? || project.pending_delete?
 
-      lease = lease_for(project_id).try_obtain
-
-      unless lease
-        log_error('Cannot obtain an exclusive lease. There must be another instance already in execution.', nil, project_id: project.id, project_path: project.full_path)
-        return
-      end
-
-      begin
-        repository_storage = project.repository_storage
-        repository_state   = project.repository_state || project.create_repository_state!
-
-        calculate_checksum(:repository, repository_storage, project.disk_path, repository_state)
-        calculate_checksum(:wiki, repository_storage, project.wiki.disk_path, repository_state)
-      ensure
-        cancel_lease_for(project_id, lease)
+      try_obtain_lease do
+        calculate_checksum(:repository, project.disk_path)
+        calculate_checksum(:wiki, project.wiki.disk_path)
       end
     end
 
     private
 
-    def calculate_checksum(type, storage, relative_path, repository_state)
-      checksum = Gitlab::Git::RepositoryChecksum.new(storage, relative_path)
+    def calculate_checksum(type, repository_relative_path)
+      checksum = Gitlab::Git::RepositoryChecksum.new(project.repository_storage, repository_relative_path)
       repository_state.update!("#{type}_verification_checksum" => checksum.calculate, "last_#{type}_verification_at" => DateTime.now)
     rescue => e
-      log_error('Error calculating the repository checksum', e, project_id: repository_state.project_id, storage: storage, relative_path: relative_path, type: type)
+      log_error('Error calculating the repository checksum', e, type: type)
       repository_state.update!("last_#{type}_verification_failure" => e.message, "last_#{type}_verification_at" => DateTime.now, "last_#{type}_verification_failed" => true)
       raise e
     end
 
-    def lease_for(project_id)
-      Gitlab::ExclusiveLease.new(lease_key(project_id), timeout: LEASE_TIMEOUT)
+    def repository_state
+      @repository_state ||= project.repository_state || project.create_repository_state!
     end
 
-    def lease_key(project_id)
-      "geo:single_repository_verification_worker:#{project_id}"
+    def lease_key
+      "geo:single_repository_verification_worker:#{project.id}"
     end
 
-    def cancel_lease_for(project_id, uuid)
-      Gitlab::ExclusiveLease.cancel(lease_key(project_id), uuid)
+    def lease_timeout
+      LEASE_TIMEOUT
     end
   end
 end
