1c20b67f48bac523d7ddc80c1d3000f2a11de96c
Jose Ivan Vargas <jvargas@gitlab.com>
2018-02-27 16:11:09 -0600

    Add persistent flash messages

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -33,6 +33,7 @@ import './milestone_select';
 import './projects_dropdown';
 import './render_gfm';
 import initBreadcrumbs from './breadcrumb';
+import { popFlashMessage } from './lib/utils/flash_queue';
 
 // EE-only scripts
 import 'ee/main';
@@ -268,4 +269,5 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 
   initDispatcher();
+  popFlashMessage();
 });
