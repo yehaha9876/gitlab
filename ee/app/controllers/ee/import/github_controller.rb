145f4a5fc903fbc9ce0983be1bdd891598959e5e
Rubén Dávila <ruben@gitlab.com>
2018-02-28 21:46:18 -0500

    Remove some code related to import from GH
    These changes will go in a different MR

--- ee/app/controllers/ee/import/github_controller.rb
+++ ee/app/controllers/ee/import/github_controller.rb
@@ -7,29 +7,6 @@ module EE
       def extra_project_attrs
         super.merge(ci_cd_only: params[:ci_cd_only])
       end
-
-      override :new_import_url
-      def new_import_url
-        append_extra_params(super)
-      end
-
-      override :status_import_url
-      def status_import_url
-        append_extra_params(super)
-      end
-
-      override :callback_import_url
-      def callback_import_url
-        append_extra_params(super)
-      end
-
-      private
-
-      def append_extra_params(url)
-        url += "?ci_cd_only=true" if params[:ci_cd_only].present?
-
-        url
-      end
     end
   end
 end
