bf19bfa40d162abbe942de9cfef3d2056887824d
Jan Provaznik <jprovaznik@gitlab.com>
2018-03-13 15:07:14 +0100

    Epic discussion thread
    Related to #3889

---
+++ app/serializers/snippet_note_entity.rb
@@ -0,0 +1,21 @@
+class SnippetNoteEntity < NoteEntity
+  expose :toggle_award_path, if: -> (note, _) { note.emoji_awardable? } do |note|
+    toggle_award_emoji_snippet_note_path(note.noteable, note)
+  end
+
+  expose :path do |note|
+    snippet_note_path(note.noteable, note)
+  end
+
+  expose :resolve_path, if: -> (note, _) { note.part_of_discussion? && note.resolvable? } do |note|
+    resolve_project_merge_request_discussion_path(note.project, note.noteable, note.discussion_id)
+  end
+
+  expose :resolve_with_issue_path, if: -> (note, _) { note.part_of_discussion? && note.resolvable? } do |note|
+    new_project_issue_path(note.project, merge_request_to_resolve_discussions_of: note.noteable.iid, discussion_to_resolve: note.discussion_id)
+  end
+
+  expose :delete_attachment_path, if: -> (note, _) { note.attachment? } do |note|
+    delete_attachment_project_note_path(note.project, note)
+  end
+end
