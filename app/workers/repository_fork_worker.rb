c8dabd24d14e06aa0fece1a3f81564b4d0f98b1d
Ahmad Sherif <me@ahmadsherif.com>
2018-03-26 20:21:49 +0200

    Use shard name in Git::GitlabProjects instead of shard path
    Related to gitaly#1110

--- app/workers/repository_fork_worker.rb
+++ app/workers/repository_fork_worker.rb
@@ -4,23 +4,40 @@ class RepositoryForkWorker
   include ProjectStartImport
   include ProjectImportOptions
 
-  def perform(project_id, forked_from_repository_storage_path, source_disk_path)
-    project = Project.find(project_id)
+  def perform(*args)
+    target_project_id = args.shift
+    target_project = Project.find(target_project_id)
 
-    return unless start_fork(project)
+    if args.any?
+      source_repository_storage_path, source_disk_path = *args
 
-    Gitlab::Metrics.add_event(:fork_repository,
-                              source_path: source_disk_path,
-                              target_path: project.disk_path)
+      source_repository_storage = Gitlab.config.repositories.storages.find do |_, info|
+        info.legacy_disk_path == source_repository_storage_path
+      end&.first || raise("no shard found for path '#{source_repository_storage_path}'")
 
-    result = gitlab_shell.fork_repository(forked_from_repository_storage_path, source_disk_path,
-                                          project.repository_storage_path, project.disk_path)
-    raise "Unable to fork project #{project_id} for repository #{source_disk_path} -> #{project.disk_path}" unless result
+      fork_repository(target_project, source_repository_storage, source_disk_path)
+    else
+      source_project = target_project.fork_source
 
-    project.after_import
+      fork_repository(target_project, source_project.repository_storage, source_project.disk_path)
+    end
   end
 
   private
+
+  def fork_repository(target_project, source_repository_storage, source_disk_path)
+    return unless start_fork(target_project)
+
+    Gitlab::Metrics.add_event(:fork_repository,
+                              source_path: source_disk_path,
+                              target_path: target_project.disk_path)
+
+    result = gitlab_shell.fork_repository(source_repository_storage, source_disk_path,
+                                          target_project.repository_storage, target_project.disk_path)
+    raise "Unable to fork project #{target_project.id} for repository #{source_disk_path} -> #{target_project.disk_path}" unless result
+
+    target_project.after_import
+  end
 
   def start_fork(project)
     return true if start(project)
