8db57c57379fd3ba76103f920eaabe4e18868a3c
Dennis Tang <dtang@gitlab.com>
2018-03-28 09:40:01 -0700

    add tooltip for total weight in milestone collapsed sidebar

--- app/helpers/milestones_helper.rb
+++ app/helpers/milestones_helper.rb
@@ -209,4 +209,12 @@ module MilestonesHelper
       group_milestone_path(@group, milestone.iid, milestone: params)
     end
   end
+
+  def milestone_weight_tooltip_text(weight)
+    if (weight.zero?)
+      _("Weight")
+    else
+      _("Weight %{weight}") % { weight: weight }
+    end
+  end
 end
