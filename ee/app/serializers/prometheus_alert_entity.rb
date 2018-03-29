b45f878e4a906d1b4c9e40de63a6cd3f4b699da4
Pawel Chojnacki <pawel.chcki@gmail.com>
2018-03-29 14:47:02 +0200

    Add path to alert

--- ee/app/serializers/prometheus_alert_entity.rb
+++ ee/app/serializers/prometheus_alert_entity.rb
@@ -8,6 +8,6 @@ class PrometheusAlertEntity < Grape::Entity
   expose :threshold
 
   expose :alert_path do |prometheus_alert|
-    project_environment_prometheus_alert_path(prometheus_alert.project, prometheus_alert.environment, prometheus_alert.iid, format: :json)
+    project_prometheus_alert_path(prometheus_alert.project, prometheus_alert.iid, format: :json)
   end
 end
