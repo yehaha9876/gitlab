2a7f1d0f944d593ca97ef1e05c951ca5ea04143d
Mike Greiling <mike@pixelcog.com>
2018-04-04 14:11:59 -0500

    add basic styling to alert dropdown form

--- app/assets/javascripts/monitoring/components/alert_widget_form.vue
+++ app/assets/javascripts/monitoring/components/alert_widget_form.vue
@@ -1,5 +1,49 @@
 <template>
-  <div>
-    form goes here...
+  <div class="alert-form">
+    <div
+      class="form-group btn-group"
+      role="group"
+      aria-label="Operator"
+    >
+      <button
+        type="button"
+        class="btn btn-default active"
+      >
+        &gt;
+      </button>
+      <button
+        type="button"
+        class="btn btn-default"
+      >
+        =
+      </button>
+      <button
+        type="button"
+        class="btn btn-default"
+      >
+        &lt;
+      </button>
+    </div>
+    <div class="form-group">
+      <label>Threshold</label>
+      <input
+        type="text"
+        class="form-control"
+      />
+    </div>
+    <div class="action-group">
+      <button
+        type="button"
+        class="btn btn-default"
+      >
+        Cancel
+      </button>
+      <button
+        type="button"
+        class="btn btn-inverted btn-new"
+      >
+        Save
+      </button>
+    </div>
   </div>
 </template>
