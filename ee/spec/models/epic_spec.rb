9ea0f5d79d8ccf4576b3da93fa0652ae84bde306
Dennis Tang <dtang@gitlab.com>
2018-04-04 22:34:41 +0200

    add tests for shiny new epic methods

--- ee/spec/models/epic_spec.rb
+++ ee/spec/models/epic_spec.rb
@@ -36,6 +36,42 @@ describe Epic do
     end
   end
 
+  describe '#upcoming?' do
+    it 'returns true' do
+      epic = build(:epic, start_date: Time.now + 1.month)
+      expect(epic.upcoming?).to be_truthy
+    end
+
+    it 'returns false' do
+      epic = build(:epic, start_date: Date.today.prev_year)
+      expect(epic.upcoming?).to be_falsey
+    end
+  end
+
+  describe '#expired?' do
+    it 'returns true' do
+      epic = build(:epic, end_date: Date.today.prev_year)
+      expect(epic.expired?).to be_truthy
+    end
+
+    it 'returns false' do
+      epic = build(:epic, end_date: Date.today.next_year)
+      expect(epic.expired?).to be_falsey
+    end
+  end
+
+  describe '#elapsed_days' do
+    it 'returns 0 if no start_date' do
+      epic = build(:epic)
+      expect(epic.elapsed_days).to eq(0)
+    end
+
+    it 'returns elapsed_days when start_date present' do
+      epic = build(:epic, start_date: 7.days.ago)
+      expect(epic.elapsed_days).to eq(7)
+    end
+  end
+
   describe '#issues' do
     let(:user) { create(:user) }
     let(:group) { create(:group, :private) }
