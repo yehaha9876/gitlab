ee63e98d63df76d2015cd4dace38786b44def39a
James Edwards-Jones <jedwardsjones@gitlab.com>
2018-04-22 22:04:56 +0100

    SamlProvider has many linked Identities

--- ee/app/models/ee/identity.rb
+++ ee/app/models/ee/identity.rb
@@ -3,7 +3,13 @@ module EE
     extend ActiveSupport::Concern
 
     prepended do
-      validates :secondary_extern_uid, allow_blank: true, uniqueness: { scope: :provider, case_sensitive: false }
+      def self.uniqueness_scope
+        [:provider, :saml_provider_id]
+      end
+
+      belongs_to :saml_provider
+
+      validates :secondary_extern_uid, allow_blank: true, uniqueness: { scope: uniqueness_scope, case_sensitive: false }
 
       scope :with_secondary_extern_uid, ->(provider, secondary_extern_uid) do
         iwhere(secondary_extern_uid: normalize_uid(provider, secondary_extern_uid)).with_provider(provider)
