266da8418e9bbde701760e37ad45d6339e11e763
Lukas Eipert <leipert@gitlab.com>
2018-05-07 20:28:19 +0200

    add initial version of epics banner

--- ee/app/assets/javascripts/pages/projects/issues/show/index.js
+++ ee/app/assets/javascripts/pages/projects/issues/show/index.js
@@ -1,9 +1,13 @@
+/* eslint-disable no-new */
+
 import initShow from '~/pages/projects/issues/show';
 import initSidebarBundle from 'ee/sidebar/sidebar_bundle';
 import initRelatedIssues from 'ee/related_issues';
+import UserCallout from '~/user_callout';
 
 document.addEventListener('DOMContentLoaded', () => {
   initShow();
   initSidebarBundle();
   initRelatedIssues();
+  new UserCallout({ className: 'js-epics-sidebar-callout' });
 });
