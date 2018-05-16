bc0a73a6fa696fbf4b5f3b98c79255b775de443f
Tiago Botelho <tiagonbotelho@hotmail.com>
2018-05-16 12:36:49 +0100

    Adds backend specs

--- app/helpers/environments_helper.rb
+++ app/helpers/environments_helper.rb
@@ -4,8 +4,4 @@ module EnvironmentsHelper
       endpoint: project_environments_path(@project, format: :json)
     }
   end
-
-  def format_kube_logs(logs)
-    logs.strip.gsub("\n", "<br>").html_safe
-  end
 end
