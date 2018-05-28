d7c76ccc60c8cb528d9527c736a289fea334de63
Tiago Botelho <tiagonbotelho@hotmail.com>
2018-05-28 11:09:02 +0100

    Adds license check to pod_logs in environments entity

--- ee/app/serializers/ee/environment_entity.rb
+++ ee/app/serializers/ee/environment_entity.rb
@@ -3,9 +3,13 @@ module EE
     extend ActiveSupport::Concern
 
     prepended do
-      expose :logs_path do |environment|
+      expose :logs_path, if: -> (*) { can_read_pod_logs? } do |environment|
         logs_project_environment_path(environment.project, environment)
       end
+    end
+
+    def can_read_pod_logs?
+      can?(current_user, :read_pod_logs, environment.project)
     end
   end
 end
