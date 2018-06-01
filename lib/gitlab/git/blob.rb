bd5c3e15455f150389e869bc780cdb03dec41b7a
Rubén Dávila <ruben@gitlab.com>
2018-05-31 23:03:23 -0500

    Improve performance used to check LFS integrity
    Instead of using `git rev-list` and loading each Blob to inspect its
    content, we're now relying on the usage of `git diff` and `git
    cat-file`. This has the main advantage
    that we're reusing the same technique used for other existing checks,
    hence all the Blobs are cached.

--- lib/gitlab/git/blob.rb
+++ lib/gitlab/git/blob.rb
@@ -85,6 +85,32 @@ module Gitlab
           end
         end
 
+        # Find LFS blobs old_rev and new_rev
+        # old_rev and new_rev are commit ID's
+        # Returns array of Gitlab::Git::Blob
+        # Does not guarantee blob data will be set
+        def lfs_pointers_between(repository, old_rev, new_rev)
+          revs_with_paths = repository.raw_changes_between(old_rev, new_rev)
+            .reject  { |c| c.deleted? }
+            .select  { |c| c.blob_size.between?(LFS_POINTER_MIN_SIZE, LFS_POINTER_MAX_SIZE) }
+            .map!    { |c| [new_rev, c.new_path] }
+
+          return [] if revs_with_paths.blank?
+
+          batch(repository, revs_with_paths, blob_size_limit: LFS_POINTER_MAX_SIZE)
+            .select(&:lfs_pointer?)
+        end
+
+        def lfs_pointers_from_tree(repository, tree)
+          revs_with_paths = tree.blobs.map! { |blob| [tree.sha, blob.path] }
+
+          return [] if revs_with_paths.blank?
+
+          batch(repository, revs_with_paths, blob_size_limit: LFS_POINTER_MAX_SIZE)
+            .select { |blob| blob.size.between?(LFS_POINTER_MIN_SIZE, LFS_POINTER_MAX_SIZE) }
+            .select(&:lfs_pointer?)
+        end
+
         def binary?(data)
           EncodingHelper.detect_libgit2_binary?(data)
         end
