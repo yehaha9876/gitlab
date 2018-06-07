c7f0bc67cf7e94bc84aa4a70ec7b2163bda05d8e
Dennis Tang <dtang@gitlab.com>
2018-06-06 19:57:39 -0700

    update the .com Registration Page to be GDPR Compliant

--- ee/app/controllers/ee/registrations_controller.rb
+++ ee/app/controllers/ee/registrations_controller.rb
@@ -2,10 +2,20 @@ module EE
   module RegistrationsController
     extend ActiveSupport::Concern
 
+    def create
+      if (params[:new_user][:terms_of_service_opted_in].present? && params[:new_user][:terms_of_service_opted_in] == '0') ||
+          (params[:new_user][:privacy_policy_opted_in].present? && params[:new_user][:privacy_policy_opted_in] == '0')
+        flash[:alert] = 'You must accept our terms of service and privacy policy in order to register an account at GitLab.com'
+        redirect_to(new_user_session_path)
+      else
+        super
+      end
+    end
+
     private
 
     def sign_up_params
-      clean_params = params.require(:user).permit(:username, :email, :email_confirmation, :name, :password, :email_opted_in)
+      clean_params = params.require(:user).permit(:username, :email, :email_confirmation, :name, :password, :email_opted_in, :terms_of_service_opted_in, :privacy_policy_opted_in)
 
       if clean_params[:email_opted_in] == '1'
         clean_params[:email_opted_in_ip] = request.remote_ip
