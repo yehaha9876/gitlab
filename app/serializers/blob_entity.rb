2959a03d83b4d08bd9e31f2c9e13be89f34d0c64
Simon Knox <psimyn@gmail.com>
2018-06-07 14:55:09 +1000

    null check for project on request

--- app/serializers/blob_entity.rb
+++ app/serializers/blob_entity.rb
@@ -13,7 +13,7 @@ class BlobEntity < Grape::Entity
     project_blob_path(request.project, File.join(request.ref, blob.path))
   end
 
-  expose :file_lock, using: FileLockEntity do |blob|
+  expose :file_lock, if: -> (*) { request.respond_to?(:project) }, using: FileLockEntity do |blob|
     if request.project.root_ref?(request.ref)
       request.project.find_path_lock(blob.path, exact_match: true)
     end
