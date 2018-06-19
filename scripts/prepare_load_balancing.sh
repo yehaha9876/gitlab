b4a47ad11e949d06a308fa43685de4a733e58d35
Stan Hu <stanhu@gmail.com>
2018-06-19 10:22:06 -0700

    Fix load balancer script

--- scripts/prepare_load_balancing.sh
+++ scripts/prepare_load_balancing.sh
@@ -3,5 +3,5 @@
 # It's primarily used to ensure that the load balancing code works with
 # multiple databases.
 if [ "$GITLAB_DATABASE" = 'postgresql' ]; then
-    sed -e '/host:/a \ \ load_balancing: \n    - postgres' config/database.yml
+    sed -e '/host:/a \ \ load_balancing: \n    - postgres' -i config/database.yml
 fi
