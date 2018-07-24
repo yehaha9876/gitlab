5a7b61ec3f562a65b77c2b112886c5759725dadb
Constance Okoghenun <constanceokoghenun@gmail.com>
2018-07-24 10:50:50 +0100

    Added boards milestone list selector dropdown

---
+++ ee/app/assets/javascripts/boards/components/assignees_list/milestones_list_item.vue
@@ -0,0 +1,33 @@
+<script>
+import { sprintf, __ } from '~/locale';
+
+export default {
+  props: {
+    list: {
+      type: Object,
+      required: true,
+    },
+  },
+  methods: {
+    handleItemClick() {
+      this.$emit('onItemSelect', this.milestone);
+    },
+  },
+};
+</script>
+
+<template>
+  <li
+    class="filter-dropdown-item"
+    @click="handleItemClick"
+  >
+    <button
+      class="btn btn-link dropdown-user"
+      type="button"
+    >
+      <div class="dropdown-user-details">
+        <div :title="list.title">{{ list.title }}</div>
+      </div>
+    </button>
+  </li>
+</template>
