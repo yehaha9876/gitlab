7f307b82cb3344238954891b67afa7b1de972a87
Brett Walker <bwalker@gitlab.com>
2018-08-07 12:40:05 -0500

    revert remove 'does not appear to be replicating the database" check

--- ee/spec/lib/gitlab/geo/health_check_spec.rb
+++ ee/spec/lib/gitlab/geo/health_check_spec.rb
@@ -15,6 +15,7 @@ describe Gitlab::Geo::HealthCheck, :geo do
       allow(described_class).to receive(:database_secondary?).and_return(true)
       allow(described_class).to receive(:get_database_version).and_return('20170101')
       allow(described_class).to receive(:get_migration_version).and_return('20170201')
+      allow(described_class).to receive(:streaming_active?).and_return(true)
 
       message = subject.perform_checks
 
@@ -53,6 +54,7 @@ describe Gitlab::Geo::HealthCheck, :geo do
     it 'returns an error when Geo database version does not match the latest migration version' do
       allow(described_class).to receive(:database_secondary?).and_return(true)
       allow(subject).to receive(:get_database_version) { 1 }
+      allow(described_class).to receive(:streaming_active?).and_return(true)
 
       expect(subject.perform_checks).to match(/Current Geo database version \([0-9]+\) does not match latest migration \([0-9]+\)/)
     end
@@ -60,19 +62,30 @@ describe Gitlab::Geo::HealthCheck, :geo do
     it 'returns an error when latest migration version does not match the Geo database version' do
       allow(described_class).to receive(:database_secondary?).and_return(true)
       allow(subject).to receive(:get_migration_version) { 1 }
+      allow(described_class).to receive(:streaming_active?).and_return(true)
 
       expect(subject.perform_checks).to match(/Current Geo database version \([0-9]+\) does not match latest migration \([0-9]+\)/)
+    end
+
+    it 'returns an error when streaming is not active and Postgresql supports pg_stat_wal_receiver' do
+      allow(Gitlab::Database).to receive(:pg_stat_wal_receiver_supported?).and_return(true)
+      allow(described_class).to receive(:database_secondary?).and_return(true)
+      allow(described_class).to receive(:streaming_active?).and_return(false)
+
+      expect(subject.perform_checks).to match(/The Geo node does not appear to be replicating the database from the primary node/)
     end
 
     it 'returns an error when streaming is not active and Postgresql does not support pg_stat_wal_receiver' do
       allow(Gitlab::Database).to receive(:pg_stat_wal_receiver_supported?).and_return(false)
       allow(described_class).to receive(:database_secondary?).and_return(true)
+      allow(described_class).to receive(:streaming_active?).and_return(false)
 
       expect(subject.perform_checks).to be_empty
     end
 
     it 'returns an error when FDW is disabled' do
       allow(described_class).to receive(:database_secondary?) { true }
+      allow(described_class).to receive(:streaming_active?) { true }
 
       allow(Gitlab::Geo::Fdw).to receive(:enabled?) { false }
 
@@ -81,6 +94,7 @@ describe Gitlab::Geo::HealthCheck, :geo do
 
     it 'returns an error when FDW remote table is not in sync but has same amount of tables' do
       allow(described_class).to receive(:database_secondary?) { true }
+      allow(described_class).to receive(:streaming_active?) { true }
 
       allow(Gitlab::Geo::Fdw).to receive(:fdw_up_to_date?) { false }
       allow(Gitlab::Geo::Fdw).to receive(:count_tables_match?) { true }
@@ -90,6 +104,7 @@ describe Gitlab::Geo::HealthCheck, :geo do
 
     it 'returns an error when FDW remote table is not in sync and has same different amount of tables' do
       allow(described_class).to receive(:database_secondary?) { true }
+      allow(described_class).to receive(:streaming_active?) { true }
 
       allow(Gitlab::Geo::Fdw).to receive(:fdw_up_to_date?) { false }
       allow(Gitlab::Geo::Fdw).to receive(:count_tables_match?) { false }
