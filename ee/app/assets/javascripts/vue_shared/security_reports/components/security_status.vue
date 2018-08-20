bb71f37c2e6f69ea9b4dd02b72ecbc43876e5034
samdbeckham <sbeckham@gitlab.com>
2018-08-20 11:40:15 +0100

    Uses constants for the icons in the security component

--- ee/app/assets/javascripts/vue_shared/security_reports/components/security_status.vue
+++ ee/app/assets/javascripts/vue_shared/security_reports/components/security_status.vue
@@ -1,15 +1,16 @@
 <script>
 import StatusIcon from '~/vue_merge_request_widget/components/mr_widget_status_icon.vue';
 import Tooltip from '~/vue_shared/directives/tooltip';
+import { ICON_WARNING, ICON_SUCCESS } from '~/reports/constants';
 
 // TODO: Stop faking the data
 // TODO: Handle a case where the commit/pipeline doesn't have security reports
 const fetchReportStatusAction = () => new Promise((resolve, reject) => {
   const dummyData = {
-    is_secure: false,
+    is_secure: true,
     pipeline_url: 'https://gitlab.com/',
   };
-  setTimeout(reject, 4000, { data: dummyData });
+  setTimeout(resolve, 4000, { data: dummyData });
 });
 
 export default {
@@ -34,15 +35,15 @@ export default {
   },
   computed: {
     status() {
-      if (this.isLoading) { return 'loading' }
-      if (this.data.is_secure) { return 'success' }
-      return 'warning';
+      if (this.isLoading) { return 'loading'; }
+      if (this.data.is_secure) { return ICON_SUCCESS; }
+      return ICON_WARNING;
     },
     hasReports() {
       // TODO: Come back to this when we have real data, this may not suffice
       // If the data object is empty, return false
       return Object.keys(this.data).length > 0;
-    }
+    },
   },
   created() {
     this.fetchReportStatus();
