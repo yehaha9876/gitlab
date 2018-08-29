jdbean/gitlab-ce

Teach Gitlab how to generate a ClusterRoleBinding as a Kubeclient::Resource
Assumes that the ClusterRole and subjects already exist

spec/lib/gitlab/kubernetes/cluster_role_binding_spec.rb
@@ -1,0 +1,21 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+describe Gitlab::Kubernetes::ClusterRoleBinding do
+  let(:name) { 'cluster-role-binding-name' }
+  let(:cluster_role_name) { 'cluster-admin' }
+  let(:subjects) { [{ kind: 'ServiceAccount', name: 'sa', namespace: 'ns' }] }
+  let(:clusterrolebinding) { described_class.new(name, cluster_role_name, subjects) }
+
+  describe '#generate' do
+    let(:roleref) { { apiGroup: 'rbac.authorization.k8s.io', kind: 'ClusterRole', name: cluster_role_name } }
+    let(:resource) { ::Kubeclient::Resource.new(metadata: { name: name }, roleRef: roleref, subjects: subjects) }
+
+    subject { clusterrolebinding.generate }
+
+    it 'should build a Kubeclient Resource' do
+      is_expected.to eq(resource)
+    end
+  end
+end

yehaha9876/gitlab

spec/lib/gitlab/kubernetes/cluster_role_binding_spec.rb
@@ -1,0 +1,21 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+describe Gitlab::Kubernetes::ClusterRoleBinding do
+  let(:name) { 'cluster-role-binding-name' }
+  let(:cluster_role_name) { 'cluster-admin' }
+  let(:subjects) { [{ kind: 'ServiceAccount', name: 'sa', namespace: 'ns' }] }
+  let(:cluster_role_binding) { described_class.new(name, cluster_role_name, subjects) }
+
+  describe '#generate' do
+    let(:role_ref) { { apiGroup: 'rbac.authorization.k8s.io', kind: 'ClusterRole', name: cluster_role_name } }
+    let(:resource) { ::Kubeclient::Resource.new(metadata: { name: name }, roleRef: role_ref, subjects: subjects) }
+
+    subject { cluster_role_binding.generate }
+
+    it 'should build a Kubeclient Resource' do
+      is_expected.to eq(resource)
+    end
+  end
+end