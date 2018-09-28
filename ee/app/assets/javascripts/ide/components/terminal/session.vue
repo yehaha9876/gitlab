c7e5c38b1e0f2813cadba95026e505958bd721c3
Paul Slaughter <pslaughter@gitlab.com>
2018-09-28 16:18:25 -0500

    Add IDE Web Terminal components

---
+++ ee/app/assets/javascripts/ide/components/terminal/session.vue
@@ -0,0 +1,60 @@
+<script>
+import { mapActions, mapState } from 'vuex';
+import { __ } from '~/locale';
+import Terminal from './terminal.vue';
+import { STATUS_RUNNING, STATUS_STARTING, STATUS_STOPPED } from '../../constants';
+
+export default {
+  components: {
+    Terminal,
+  },
+  computed: {
+    ...mapState('terminal', ['session']),
+    actionButton() {
+      if (!this.session) {
+        return null;
+      }
+
+      const { status } = this.session;
+
+      if (status === STATUS_STOPPED) {
+        return {
+          action: () => this.restartSession(),
+          text: __('Restart Terminal'),
+          class: 'btn-primary',
+        };
+      } else if (status === STATUS_STARTING || status === STATUS_RUNNING) {
+        return {
+          action: () => this.stopSession(),
+          text: __('Stop Terminal'),
+          class: 'btn-inverted btn-remove',
+        };
+      }
+
+      return null;
+    },
+  },
+  methods: {
+    ...mapActions('terminal', ['restartSession', 'stopSession']),
+  },
+};
+</script>
+
+<template>
+  <div class="ide-terminal build-page d-flex flex-column">
+    <header class="ide-job-header d-flex align-items-center">
+      <h5>{{ __('Web Terminal') }}</h5>
+      <div class="ml-auto align-self-center">
+        <button
+          v-if="actionButton"
+          type="button"
+          class="btn btn-sm"
+          :class="actionButton.class"
+          @click="actionButton.action"
+          v-text="actionButton.text"
+        ></button>
+      </div>
+    </header>
+    <terminal :status="session.status" :terminal-build-path="session.terminalPath" />
+  </div>
+</template>
--- ee/app/assets/javascripts/ide/components/terminal/view.vue
+++ ee/app/assets/javascripts/ide/components/terminal/view.vue
@@ -1,21 +1,41 @@
 <script>
-import { mapState } from 'vuex';
+import { mapActions, mapGetters, mapState } from 'vuex';
+import TerminalSession from './session.vue';
 import EmptyState from './empty_state.vue';
 
 export default {
   components: {
     EmptyState,
+    TerminalSession,
   },
   computed: {
-    ...mapState(['emptyStateSvgPath']),
+    ...mapState('terminal', ['isShowSplash', 'paths']),
+    ...mapGetters('terminal', ['environmentCheck']),
+  },
+  methods: {
+    ...mapActions('terminal', ['startSession', 'hideSplash']),
+    start() {
+      this.hideSplash();
+      this.startSession();
+    },
   },
 };
 </script>
 
 <template>
   <div class="h-100">
-    <div class="h-100 d-flex flex-column justify-content-center">
-      <empty-state :illustration-path="emptyStateSvgPath" />
+    <div v-if="isShowSplash" class="h-100 d-flex flex-column justify-content-center">
+      <empty-state
+        :is-loading="environmentCheck.isLoading"
+        :is-valid="environmentCheck.isValid"
+        :message="environmentCheck.message"
+        :help-path="paths.webTerminalHelpPath"
+        :illustration-path="paths.webTerminalSvgPath"
+        @start="start();"
+      />
     </div>
+    <template v-else>
+      <terminal-session />
+    </template>
   </div>
 </template>
