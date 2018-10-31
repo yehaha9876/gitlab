079f222db84af0f9963daedf99324f648c0abe2c
Clement Ho <ClemMakesApps@gmail.com>
2018-10-31 12:46:38 -0500

    Remove gitlab-ui's tooltip from global

--- app/assets/javascripts/commons/gitlab_ui.js
+++ app/assets/javascripts/commons/gitlab_ui.js
@@ -3,11 +3,8 @@ import {
   GlPagination,
   GlProgressBar,
   GlLoadingIcon,
-  GlTooltipDirective,
 } from '@gitlab-org/gitlab-ui';
 
 Vue.component('gl-pagination', GlPagination);
 Vue.component('gl-progress-bar', GlProgressBar);
 Vue.component('gl-loading-icon', GlLoadingIcon);
-
-Vue.directive('gl-tooltip', GlTooltipDirective);
