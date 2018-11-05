b7b2331bbec4949bf79092563541be725f882b3d
Gilbert Roulot <groulot@gitlab.com>
2018-11-05 14:45:36 +0100

    Use DRY in test and license reports services

--- ee/app/services/ci/compare_license_management_reports_service.rb
+++ ee/app/services/ci/compare_license_management_reports_service.rb
@@ -1,39 +1,17 @@
 # frozen_string_literal: true
 
 module Ci
-  class CompareLicenseManagementReportsService < ::BaseService
-    def execute(base_pipeline, head_pipeline)
-      # rubocop: disable CodeReuse/Serializer
-      comparer = Gitlab::Ci::Reports::LicenseManagementReportsComparer
-        .new(base_pipeline&.license_management_report, head_pipeline.license_management_report)
-
-      {
-        status: :parsed,
-        key: key(base_pipeline, head_pipeline),
-        data: LicenseManagementReportsComparerSerializer
-          .new(project: project)
-          .represent(comparer).as_json
-      }
-    rescue => e
-      {
-        status: :error,
-        key: key(base_pipeline, head_pipeline),
-        status_reason: e.message
-      }
-      # rubocop: enable CodeReuse/Serializer
+  class CompareLicenseManagementReportsService < ::Ci::CompareReportsBaseService
+    def comparer_class
+      Gitlab::Ci::Reports::LicenseManagementReportsComparer
     end
 
-    def latest?(base_pipeline, head_pipeline, data)
-      data&.fetch(:key, nil) == key(base_pipeline, head_pipeline)
+    def serializer_class
+      LicenseManagementReportsComparerSerializer
     end
 
-    private
-
-    def key(base_pipeline, head_pipeline)
-      [
-        base_pipeline&.id, base_pipeline&.updated_at,
-        head_pipeline&.id, head_pipeline&.updated_at
-      ]
+    def get_report(pipeline)
+      pipeline&.license_management_report
     end
   end
 end
