b15fe0e75739567a6aaed8d1e984c3090229b19e
Sanad Liaquat <sliaquat@gitlab.com>
2019-01-10 20:00:17 +0500

    Some refactoring

--- qa/qa/specs/features/browser_ui/1_manage/project/ee_instance_level_project_template_spec.rb
+++ qa/qa/specs/features/browser_ui/1_manage/project/ee_instance_level_project_template_spec.rb
@@ -3,8 +3,7 @@ require 'securerandom'
 
 module QA
   context :manage do
-    describe 'Project template' do
-
+    describe 'Project templates' do
       before(:all) do
         @files = [
             {
@@ -16,6 +15,7 @@ module QA
                 content: 'bar'
             }
         ]
+
         Runtime::Browser.visit(:gitlab, Page::Main::Login)
         Page::Main::Login.perform(&:sign_in_using_credentials)
 
@@ -38,83 +38,93 @@ module QA
         end
       end
 
-      it 'user creates and uses instance level project template' do
-        Runtime::Browser.visit(:gitlab, Page::Main::Login)
-        Page::Main::Login.perform(&:sign_in_using_credentials)
+      context 'instance level' do
+        before do
+          Runtime::Browser.visit(:gitlab, Page::Main::Login)
+          Page::Main::Login.perform(&:sign_in_using_credentials)
 
-        Page::Main::Menu.perform(&:go_to_admin_area)
-        Page::Admin::Menu.perform(&:go_to_template_settings)
+          Page::Main::Menu.perform(&:go_to_admin_area)
+          Page::Admin::Menu.perform(&:go_to_template_settings)
 
-        EE::Page::Admin::Settings::Templates.perform do |page|
-          page.choose_custom_project_template("#{@template_container_group_name}")
+          EE::Page::Admin::Settings::Templates.perform do |page|
+            page.choose_custom_project_template("#{@template_container_group_name}")
+          end
+
+          group = Resource::Group.fabricate_via_api!
+          group.visit!
+
+          Page::Group::Show.perform(&:go_to_new_project)
+
+          Page::Project::New.perform(&:go_to_create_from_template_instance_tab)
         end
 
-        group = Resource::Group.fabricate_via_api!
-        group.visit!
+        it 'successfully imports the project using template' do
+          Page::Project::New.perform do |page|
+            expect(page.instance_template_tab_badge_text).to eq "1"
+            expect(page).to have_text(@template_project.name)
+          end
 
-        Page::Group::Show.perform(&:go_to_new_project)
+          create_project_using_template(project_name: 'Project using instance level project template',
+                                        namespace: Runtime::Namespace.path,
+                                        template_name: @template_project.name)
 
-        Page::Project::New.perform do |page|
-          page.go_to_create_from_template_instance_tab
-
-          expect(page.instance_template_tab_badge_text).to eq "1"
-          expect(page).to have_text(@template_project.name)
-
-          page.use_template_for_project(@template_project.name)
-
-          page.choose_namespace(Runtime::Namespace.path)
-          page.choose_name("Project using group level project template #{SecureRandom.hex(8)}")
-          page.add_description('Project using instance level project template')
-          page.set_visibility('Public')
-          page.create_new_project
-        end
-
-        Page::Project::Show.perform(&:wait_for_import_success)
-
-        @files.each do |file|
-          expect(page).to have_content(file[:name])
+          Page::Project::Show.perform(&:wait_for_import_success)
+          @files.each do |file|
+            expect(page).to have_content(file[:name])
+          end
         end
       end
 
-      it 'user creates and uses group level project template' do
-        Runtime::Browser.visit(:gitlab, Page::Main::Login)
-        Page::Main::Login.perform(&:sign_in_using_credentials)
+      context 'group level' do
+        before do
+          Runtime::Browser.visit(:gitlab, Page::Main::Login)
+          Page::Main::Login.perform(&:sign_in_using_credentials)
 
-        Page::Main::Menu.perform(&:go_to_groups)
-        Page::Dashboard::Groups.perform { |page| page.go_to_group(Runtime::Namespace.sandbox_name) }
-        Page::Project::Menu.perform(&:go_to_settings)
+          Page::Main::Menu.perform(&:go_to_groups)
+          Page::Dashboard::Groups.perform { |page| page.go_to_group(Runtime::Namespace.sandbox_name) }
+          Page::Project::Menu.perform(&:go_to_settings)
 
-        EE::Page::Group::Settings::General.perform do |settings|
-          settings.choose_custom_project_template("#{@template_container_group_name}")
+          EE::Page::Group::Settings::General.perform do |settings|
+            settings.choose_custom_project_template("#{@template_container_group_name}")
+          end
+
+          group = Resource::Group.fabricate_via_api!
+          group.visit!
+
+          Page::Group::Show.perform(&:go_to_new_project)
+
+          Page::Project::New.perform(&:go_to_create_from_template_group_tab)
         end
 
-        group = Resource::Group.fabricate_via_api!
-        group.visit!
+        it 'successfully imports the project using template' do
+          Page::Project::New.perform do |page|
+            expect(page.group_template_tab_badge_text).to eq "1"
+            expect(page).to have_text(@template_container_group_name)
+            expect(page).to have_text(@template_project.name)
+          end
 
-        Page::Group::Show.perform(&:go_to_new_project)
+          create_project_using_template(project_name: 'Project using group level project template',
+                                        namespace: Runtime::Namespace.sandbox_name,
+                                        template_name: @template_project.name)
 
+          Page::Project::Show.perform(&:wait_for_import_success)
+          @files.each do |file|
+            expect(page).to have_content(file[:name])
+          end
+        end
+      end
+
+      def create_project_using_template(project_name:, namespace:, template_name:)
         Page::Project::New.perform do |page|
-          page.go_to_create_from_template_group_tab
-
-          expect(page.group_template_tab_badge_text).to eq "1"
-          expect(page).to have_text(@template_container_group_name)
-          expect(page).to have_text(@template_project.name)
-
-          page.use_template_for_project(@template_project.name)
-
-          page.choose_namespace(Runtime::Namespace.sandbox_name)
-          page.choose_name("Project using group level project template #{SecureRandom.hex(8)}")
-          page.add_description('Project using group level project template')
+          page.use_template_for_project(template_name)
+          page.choose_namespace(namespace)
+          page.choose_name("#{project_name} #{SecureRandom.hex(8)}")
+          page.add_description("#{project_name}")
           page.set_visibility('Public')
           page.create_new_project
-        end
-
-        Page::Project::Show.perform(&:wait_for_import_success)
-
-        @files.each do |file|
-          expect(page).to have_content(file[:name])
         end
       end
     end
   end
 end
+
