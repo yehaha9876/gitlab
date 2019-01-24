7e427eb4772eb369b1e5d3296d33473c90b232b6
Thong Kuah <tkuah@gitlab.com>
2019-01-25 09:25:34 +1300

    Make existing EE migration idempotent
    We added a copy of this migration in CE with a later timestamp so we
    need this migration to be able to run successfully in conjuction with
    that copied migration

--- ee/db/migrate/20180524115107_add_last_update_started_at_to_applications_prometheus.rb
+++ ee/db/migrate/20180524115107_add_last_update_started_at_to_applications_prometheus.rb
@@ -1,7 +1,15 @@
 class AddLastUpdateStartedAtToApplicationsPrometheus < ActiveRecord::Migration[4.2]
   DOWNTIME = false
 
-  def change
-    add_column :clusters_applications_prometheus, :last_update_started_at, :datetime_with_timezone
+  def up
+    unless column_exists?(:clusters_applications_prometheus, :last_update_started_at)
+      add_column :clusters_applications_prometheus, :last_update_started_at, :datetime_with_timezone
+    end
+  end
+
+  def down
+    if column_exists?(:clusters_applications_prometheus, :last_update_started_at)
+      remove_column(:clusters_applications_prometheus, :last_update_started_at)
+    end
   end
 end
