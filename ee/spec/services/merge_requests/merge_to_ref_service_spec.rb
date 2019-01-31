00363213f6b4815dff9a38406e24f5f355360d11
Oswaldo Ferreira <oswaldo@gitlab.com>
2019-01-31 16:32:44 -0200

    Support merge to ref for merge-commit and squash
    Adds the ground work for writing into
    the merge ref refs/merge-requests/:iid/merge the
    merge result between source and target branches of
    a MR, without further side-effects such as
    mailing, MR updates and target branch changes.

--- ee/spec/services/merge_requests/merge_to_ref_service_spec.rb
+++ ee/spec/services/merge_requests/merge_to_ref_service_spec.rb
@@ -1,6 +1,8 @@
+# frozen_string_literal: true
+
 require 'spec_helper'
 
-describe MergeRequests::MergeService do
+describe MergeRequests::MergeToRefService do
   let(:user) { create(:user) }
   let(:merge_request) { create(:merge_request, :simple) }
   let(:project) { merge_request.project }
@@ -16,13 +18,18 @@ describe MergeRequests::MergeService do
         allow(project).to receive(:above_size_limit?).and_return(true)
       end
 
-      it 'persists the correct error message' do
-        service.execute(merge_request)
+      it 'returns the correct error message' do
+        result = service.execute(merge_request)
 
-        expect(merge_request.merge_error).to include('This merge request cannot be merged')
+        expected_error =
+          'This merge request cannot be merged, because this repository ' \
+          'has exceeded its size limit'
+
+        expect(result[:status]).to eq(:error)
+        expect(result[:message]).to start_with(expected_error)
       end
     end
   end
 
-  it_behaves_like 'merge validation hooks', persisted: true
+  it_behaves_like 'merge validation hooks', persisted: false
 end
