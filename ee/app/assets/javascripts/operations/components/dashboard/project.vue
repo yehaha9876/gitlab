787e261b2579646e1110a3d049aaed766142c1e9
Scott Hampton <shampton@gitlab.com>
2019-02-04 14:14:24 -0700

    Internationalization of empty state
    Adding locale to the empty state messages

--- ee/app/assets/javascripts/operations/components/dashboard/project.vue
+++ ee/app/assets/javascripts/operations/components/dashboard/project.vue
@@ -1,23 +1,24 @@
 <script>
 import { mapActions } from 'vuex';
+import { __ } from '~/locale';
 import timeago from '~/vue_shared/mixins/timeago';
 import Icon from '~/vue_shared/components/icon.vue';
+import Commit from '~/vue_shared/components/commit.vue';
 import UserAvatarLink from '~/vue_shared/components/user_avatar/user_avatar_link.vue';
 import TimeAgo from './time_ago.vue';
 import DashboardAlerts from './alerts.vue';
 import ProjectHeader from './project_header.vue';
 import ProjectPipeline from './project_pipeline.vue';
-import CommitComponent from '~/vue_shared/components/commit.vue';
 
 export default {
   components: {
     Icon,
+    Commit,
     DashboardAlerts,
     ProjectHeader,
     ProjectPipeline,
     TimeAgo,
     UserAvatarLink,
-    CommitComponent,
   },
   mixins: [timeago],
   props: {
@@ -69,6 +70,12 @@ export default {
         ref_url: this.project.last_deployment.ref.ref_path,
       };
     },
+    noPipelineMessage() {
+      return __('The branch for this project has no active pipeline configuration.');
+    },
+    emptyProjectMessage() {
+      return __('The branch for this project is empty');
+    },
   },
   methods: {
     ...mapActions(['removeProject']),
@@ -87,12 +94,12 @@ export default {
     <div :class="cardClasses" class="ops-dashboard-project-card-body card-body">
       <div v-if="!hasLastDeployment" class="h-100 d-flex justify-content-center align-items-center">
         <div class="text-plain text-center bold w-75 ops-dashboard-project-empty-state">
-          The branch for this project has no active pipeline configuration.
+          {{ noPipelineMessage }}
         </div>
       </div>
       <div v-else-if="!hasCommit" class="h-100 d-flex justify-content-center align-items-center">
         <div class="text-plain text-center bold w-75 ops-dashboard-project-empty-state">
-          The branch for this project is empty
+          {{ emptyProjectMessage }}
         </div>
       </div>
       <div v-else>
@@ -108,12 +115,12 @@ export default {
           </div>
 
           <div class="col-6 pr-0 align-self-center ops-dashboard-project-commit">
-            <commit-component
+            <commit
               v-if="project.last_deployment"
               :tag="project.last_deployment.tag"
               :commit-ref="commitRef"
-              :commit-url="project.last_deployment.commit.commit_url"
               :short-sha="project.last_deployment.commit.short_id"
+              :commit-url="project.last_deployment.commit.commit_url"
               :title="project.last_deployment.commit.title"
               :author="project.last_deployment.commit.author"
               :show-branch="true"
