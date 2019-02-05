f852e07a2e28b4a8888d220ef19252797650e91a
Tetiana Chupryna <tchupryna@gitlab.com>
2019-02-05 23:00:14 +0200

    Add pipelines to seeds

--- scripts/review_apps/review-apps.sh
+++ scripts/review_apps/review-apps.sh
@@ -287,7 +287,7 @@ function add_secure_seeds() {
   if [ -z "${task_runner_pod}" ]; then echo "Task runner pod not found" && return; fi
 
   kubectl -n "$KUBE_NAMESPACE" exec ${task_runner_pod} -i -t -- bash -c \
-   'cd /srv/gitlab && bundle exec rake db:seed_fu FILTER=vulnerabilities FIXTURE_PATH=/db/fixtures/development'
+   'cd /srv/gitlab && bundle exec rake db:seed_fu FILTER=vulnerabilities,pipelines FIXTURE_PATH=/db/fixtures/development'
 
   kubectl -n "$KUBE_NAMESPACE" exec -i ${task_runner_pod} -- /srv/gitlab/bin/rails runner -e production 'puts "Added #{Vulnerabilities::Occurrence.count} vulnerabilities"'
 }
