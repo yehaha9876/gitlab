9a14b3cdcf86feb926641dc7b85f9f1c7a1c7101
Igor Drozdov <idrozdov@gitlab.com>
2019-02-06 13:01:54 +0300

    Add feature spec

---
+++ spec/features/merge_requests/user_filters_by_approvers_spec.rb
@@ -0,0 +1,60 @@
+require 'rails_helper'
+
+describe 'Merge Requests > User filters by approvers', :js do
+  include FilteredSearchHelpers
+
+  let(:project) { create(:project, :public, :repository) }
+  let(:user)    { project.creator }
+
+  let!(:merge_request_with_approver) do
+    create(:merge_request_with_approver, title: 'Bugfix1', source_project: project, target_project: project, source_branch: 'bugfix1')
+  end
+  let!(:second_merge_request_with_approver) do
+    create(:merge_request_with_approver, title: 'Bugfix2', source_project: project, target_project: project, source_branch: 'bugfix2')
+  end
+  let!(:merge_request) do
+    create(:merge_request, title: 'Bugfix3', source_project: project, target_project: project, source_branch: 'bugfix3')
+  end
+
+  let(:first_user) { merge_request_with_approver.approvers.first.user }
+  let(:second_user) { second_merge_request_with_approver.approvers.first.user }
+
+  before do
+    sign_in(user)
+    visit project_merge_requests_path(project)
+  end
+
+  context 'filtering by approver:none' do
+    it 'applies the filter' do
+      input_filtered_search('approver:none')
+
+      expect(page).to have_issuable_counts(open: 1, closed: 0, all: 1)
+
+      expect(page).not_to have_content 'Bugfix1'
+      expect(page).not_to have_content 'Bugfix2'
+      expect(page).to have_content 'Bugfix3'
+    end
+  end
+
+  context 'filtering by assignee:@username' do
+    it 'applies the filter' do
+      input_filtered_search("approver:@#{first_user.username}")
+
+      expect(page).to have_issuable_counts(open: 1, closed: 0, all: 1)
+      expect(page).to have_content 'Bugfix1'
+      expect(page).not_to have_content 'Bugfix2'
+      expect(page).not_to have_content 'Bugfix3'
+    end
+  end
+
+  context 'filtering by multiple approvers' do
+    it 'applies the filter' do
+      input_filtered_search("approver:@#{first_user.username} approver:@#{second_user.username}")
+
+      expect(page).to have_issuable_counts(open: 2, closed: 0, all: 2)
+      expect(page).to have_content 'Bugfix1'
+      expect(page).to have_content 'Bugfix2'
+      expect(page).not_to have_content 'Bugfix3'
+    end
+  end
+end
