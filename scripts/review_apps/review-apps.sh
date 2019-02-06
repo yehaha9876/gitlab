c6e4ad49f6da94a72223565b48b04278fa72cb86
Tetiana Chupryna <tchupryna@gitlab.com>
2019-02-06 15:30:28 +0200

    Check how many projects we have

--- scripts/review_apps/review-apps.sh
+++ scripts/review_apps/review-apps.sh
@@ -286,6 +286,8 @@ function add_secure_seeds() {
   task_runner_pod=$(get_pod "task-runner");
   if [ -z "${task_runner_pod}" ]; then echo "Task runner pod not found" && return; fi
 
+kubectl -n "$KUBE_NAMESPACE" exec -i ${task_runner_pod} -- /srv/gitlab/bin/rails runner -e production 'puts "With #{Project.count} projects"'
+
   kubectl -n "$KUBE_NAMESPACE" exec ${task_runner_pod} -i -t -- bash -c \
    'cd /srv/gitlab && bundle exec rake db:seed_fu FILTER=vulnerabilities,pipelines FIXTURE_PATH=/db/fixtures/development'
 
