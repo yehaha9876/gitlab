06e54e5701a915e126d36a2ec4efc5ccebafd0cc
Victor Zagorodny <vzagorodny@gitlab.com>
2019-02-08 16:18:11 +0200

    Skip passing store and router to UnavailableState

--- ee/app/assets/javascripts/security_dashboard/index.js
+++ ee/app/assets/javascripts/security_dashboard/index.js
@@ -4,47 +4,42 @@ import UnavailableState from './components/unavailable_state.vue';
 import createStore from './store';
 import router from './store/router';
 
-function createInstance({ el, components, name, props }) {
-  const store = createStore();
+export default () => {
+  let el = document.getElementById('js-group-security-dashboard-unavailable');
 
+  if (el) {
+    return new Vue({
+      el,
+      components: { UnavailableState },
+      render(createElement) {
+        return createElement('unavailable-state', { props: {
+            link: el.dataset.dashboardDocumentation,
+            svgPath: el.dataset.emptyStateSvgPath,
+          } });
+      },
+    });
+  }
+
+  el = document.getElementById('js-group-security-dashboard')
+  const store = createStore();
   return new Vue({
     el,
     store,
     router,
-    components,
+    components: { GroupSecurityDashboardApp },
     render(createElement) {
-      return createElement(name, { props });
-    },
-  });
-}
+      return createElement('group-security-dashboard-app', { props: {
+          dashboardDocumentation: el.dataset.dashboardDocumentation,
+          emptyStateSvgPath: el.dataset.emptyStateSvgPath,
+          projectsEndpoint: el.dataset.projectsEndpoint,
+          vulnerabilityFeedbackHelpPath: el.dataset.vulnerabilityFeedbackHelpPath,
+          vulnerabilitiesEndpoint: el.dataset.vulnerabilitiesEndpoint,
+          vulnerabilitiesCountEndpoint: el.dataset.vulnerabilitiesSummaryEndpoint,
+          vulnerabilitiesHistoryEndpoint: el.dataset.vulnerabilitiesHistoryEndpoint,
+        } });
+    }
+  })
 
-export default () => {
-  let el = document.getElementById('js-group-security-dashboard-unavailable');
-  let components;
-  let props;
-  let name;
 
-  if (el) {
-    components = { UnavailableState };
-    name = 'unavailable-state';
-    props = {
-      link: el.dataset.dashboardDocumentation,
-      svgPath: el.dataset.emptyStateSvgPath,
-    };
-  } else {
-    el = document.getElementById('js-group-security-dashboard');
-    components = { GroupSecurityDashboardApp };
-    name = 'group-security-dashboard-app';
-    props = {
-      dashboardDocumentation: el.dataset.dashboardDocumentation,
-      emptyStateSvgPath: el.dataset.emptyStateSvgPath,
-      projectsEndpoint: el.dataset.projectsEndpoint,
-      vulnerabilityFeedbackHelpPath: el.dataset.vulnerabilityFeedbackHelpPath,
-      vulnerabilitiesEndpoint: el.dataset.vulnerabilitiesEndpoint,
-      vulnerabilitiesCountEndpoint: el.dataset.vulnerabilitiesSummaryEndpoint,
-      vulnerabilitiesHistoryEndpoint: el.dataset.vulnerabilitiesHistoryEndpoint,
-    };
-  }
 
-  return createInstance({ el, components, name, props });
 };
