8ff4a3502008ef8a1a9752629804c1bdf3fb2b1e
Vladimir Shushlin <v.shushlin@gmail.com>
2019-02-14 16:03:52 +0300

    Add more specs for FeatureFlags::UpdateService [ci-skip]

--- ee/spec/services/ee/feature_flags/update_service_spec.rb
+++ ee/spec/services/ee/feature_flags/update_service_spec.rb
@@ -18,6 +18,11 @@ describe EE::FeatureFlags::UpdateService do
       it { expect { subject }.not_to change { AuditEvent.count } }
     end
 
+    shared_examples 'successfully updates' do
+      it { expect(result).to eq(true) }
+      it { expect(returned_feature_flag).to be_valid }
+    end
+
     context 'when add scope' do
       let(:params) do
         {
@@ -25,8 +30,7 @@ describe EE::FeatureFlags::UpdateService do
         }
       end
 
-      it { expect(result).to eq(true) }
-      it { expect(returned_feature_flag).to be_valid }
+      include_examples 'successfully updates'
       it { expect { subject }.to change { feature_flag.scopes.count }.by(1) }
       it { expect { subject }.to change { AuditEvent.count }.by(1) }
 
@@ -34,6 +38,50 @@ describe EE::FeatureFlags::UpdateService do
         subject
         expect(AuditEvent.last.details).to(
           include(created_feature_flag_rule: 'production', and_set_it_as: 'inactive')
+        )
+      end
+    end
+
+    context 'when changing scope value' do
+      let(:params) do
+        {
+          scopes_attributes: [{ id: feature_flag.scopes.first.id, environment_scope: '*', active: false }]
+        }
+      end
+
+      include_examples 'successfully updates'
+      it { expect { subject }.not_to change { feature_flag.scopes.count } }
+      it { expect { subject }.to change { AuditEvent.count }.by(1) }
+
+      it 'creates audit event' do
+        subject
+        expect(AuditEvent.last.details).to(
+          include(updated_feature_flag_rule: '*', and_set_it_as: 'inactive')
+        )
+      end
+    end
+
+    context 'when renaming scope' do
+      let!(:scope) do
+        feature_flag.scopes.create!(environment_scope: 'staging', active: true)
+      end
+      let(:params) do
+        {
+          scopes_attributes: [{ id: scope.id, environment_scope: 'staging/*', active: true }]
+        }
+      end
+
+      include_examples 'successfully updates'
+      it { expect { subject }.not_to change { feature_flag.scopes.count } }
+      it { expect { subject }.to change { AuditEvent.count }.by(2) }
+
+      it 'creates audit event' do
+        subject
+        expect(AuditEvent.all.map(&:details)).to(
+          contain_exactly(
+            include(deleted_feature_flag_rule: 'staging'),
+            include(created_feature_flag_rule: 'staging/*', and_set_it_as: 'active')
+          )
         )
       end
     end
